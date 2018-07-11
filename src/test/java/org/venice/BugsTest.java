5e3e102254ecffa29fd39e8852d0ba1d0c63c994
juerg <juerg@10.0.1.6>
2018-07-11 12:02:36 +0200

    refactoring

--- src/test/java/org/venice/BugsTest.java
+++ src/test/java/org/venice/BugsTest.java
@@ -7,7 +7,6 @@ import org.venice.impl.CoreFunctions;
 import org.venice.impl.Env;
 import org.venice.impl.VeniceInterpreter;
 import org.venice.impl.types.Constants;
-import org.venice.impl.types.VncFunction;
 import org.venice.impl.types.VncSymbol;
 import org.venice.impl.types.VncVal;
 
@@ -15,15 +14,13 @@ import org.venice.impl.types.VncVal;
 public class BugsTest {
 
 	@Test
-	public void test_meta_for_vectors() {
-	
+	public void test_meta_for_vectors() {	
 		// Create a VeniceInterpreter without 'core.vnc' for simpler testing
 		final VeniceInterpreter venice = new VeniceInterpreter();
 		final Env env = new Env(null);
 		
 		// Add the 'meta' function
-		final VncFunction meta = CoreFunctions.meta;
-		env.set(new VncSymbol(meta.getName()), meta);
+		env.set(new VncSymbol(CoreFunctions.meta.getName()), CoreFunctions.meta);
 		
 		// Test...
 		final VncVal result = venice.RE("(meta [1 2 3])", null, env);		
