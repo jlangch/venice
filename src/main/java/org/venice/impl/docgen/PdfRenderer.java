b5754142e0529d1a6180350ba1b892da4f4dfb62
juerg <juerg@10.0.1.6>
2018-07-17 09:16:12 +0200

    updated PdfRenderer

--- src/main/java/org/venice/impl/docgen/PdfRenderer.java
+++ src/main/java/org/venice/impl/docgen/PdfRenderer.java
@@ -29,13 +29,10 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 public class PdfRenderer {
 
 	public static byte[] renderCheatSheet(final String xhtml) {
-		try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-			
+		try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {			
 			final ITextRenderer renderer = new ITextRenderer(DOTS_PER_POINT, DOTS_PER_PIXEL);
 
 			renderer.setDocumentFromString(xhtml, "classpath:///");
-//			final org.w3c.dom.Document doc = parseXHTML(xhtml);
-//			renderer.setDocument(doc, "classpath:///");
 			renderer.layout();
 			renderer.createPDF(os);
 			os.flush();	
@@ -47,27 +44,6 @@ public class PdfRenderer {
 		}		
 	}
 
-
-//	private org.w3c.dom.Document parseXHTML(final String xhtml) throws Exception {
-//		try {
-//			final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-//			return builder.parse(new ByteArrayInputStream(xhtml.getBytes("UTF-8")));
-//		}
-//		catch(SAXParseException ex) {
-//			// Get an extract of the failure to simplify error analysis (+/- 10 lines)
-//			throw new RuntimeException(
-//					String.format(
-//							"Invalid XHTML template regarding XML validation.\nXML parser error: %s",
-//							ex.getMessage()), 
-//					ex);
-//		}		
-//		catch(Exception ex) {
-//			throw new RuntimeException("Failed to parse XHTML template", ex);
-//		}
-//	}
-
-	// These two defaults combine to produce an effective resolution of 96 px to
-    // the inch
     private static final int DOTS_PER_PIXEL = 20;
     private static final float DOTS_PER_POINT = (float)DOTS_PER_PIXEL * 96f / 72f;
 }
