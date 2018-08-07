2c83306bef7100c12cbee7d627f1495b516b6574
juerg <juerg@172.20.10.8>
2018-08-07 11:33:33 +0200

    worked on xchart extension module (bubble charts)

--- src/test/java/com/github/jlangch/venice/modules/XChartModuleTest.java
+++ src/test/java/com/github/jlangch/venice/modules/XChartModuleTest.java
@@ -34,24 +34,24 @@ public class XChartModuleTest {
 
 		final String script =
 			"(do                                                                   \n" +
-			"	(load-module :xchart)                                              \n" +
+			"   (load-module :xchart)                                              \n" +
 			"                                                                      \n" +
-			"	(xchart/swing-view-chart                                           \n" +
-			"	   (xchart/xy-chart                                                \n" +
-			"	       { \"Maxime\" { :x (range 10)                                \n" +
-			"	                      :y (mapv (fn [x] (+ x (rand-double 3.0)))    \n" +
-			"	                             (range 10)) }                         \n" +
+			"   (xchart/swing-view-chart                                           \n" +
+			"      (xchart/xy-chart                                                \n" +
+			"          { \"Maxime\" { :x (range 10)                                \n" +
+			"                         :y (mapv (fn [x] (+ x (rand-double 3.0)))    \n" +
+			"                                (range 10)) }                         \n" +
 			"            \"Tyrone\" { :x (range 10)                                \n" +
-			"	                      :y (mapv (fn [x] (+ 2 x (rand-double 4.0)))  \n" +
-			"	                            (range 0 5 0.5)) }}                    \n" +
+			"                         :y (mapv (fn [x] (+ 2 x (rand-double 4.0)))  \n" +
+			"                               (range 0 5 0.5)) }}                    \n" +
 			"                                                                      \n" +
-			"	      { :title \"Longest running distance\"                        \n" +
-			"	        :x-axis { :title \"Months (since start)\" }                \n" +
-			"	        :y-axis { :title \"Distance\"                              \n" +
-			"	                  :decimal-pattern \"##.## km\" }                  \n" +
-			"	        :theme :matlab } ))                                        \n" +
-			"	                                                                   \n" +
-			"	 (sleep 20000)                                                     \n" +
+			"         { :title \"Longest running distance\"                        \n" +
+			"           :x-axis { :title \"Months (since start)\" }                \n" +
+			"           :y-axis { :title \"Distance\"                              \n" +
+			"                     :decimal-pattern \"##.## km\" }                  \n" +
+			"           :theme :matlab } ))                                        \n" +
+			"                                                                      \n" +
+			"    (sleep 20000)                                                     \n" +
 			") ";
 		
 		System.out.println(venice.eval("(str " + script + ")"));
@@ -63,42 +63,56 @@ public class XChartModuleTest {
 
 		final String script =
 			"(do                                                                   \n" +
-			"	(load-module :xchart)                                              \n" +
+			"   (load-module :xchart)                                              \n" +
 			"                                                                      \n" +
-			"	(xchart/swing-view-chart                                           \n" +
-			"	   (xchart/pie-chart                                               \n" +
-			"	       { \"Spaces\" 400                                            \n" + 
-			"            \"Tabs\" 310                                              \n" + 
+			"   (xchart/swing-view-chart                                           \n" +
+			"      (xchart/pie-chart                                               \n" +
+			"          { \"Spaces\" 400                                            \n" +
+			"            \"Tabs\" 310                                              \n" +
 			"            \"A mix of both\" 50 }))                                  \n" +
-			"	                                                                   \n" +
-			"	 (sleep 20000)                                                     \n" +
+			"                                                                      \n" +
+			"    (sleep 20000)                                                     \n" +
 			") ";
 		
 		System.out.println(venice.eval("(str " + script + ")"));
 	}
 
 	@Test
-	public void test_pie_chart_donut() {
+	public void test_bubble_chart() {
 		final Venice venice = new Venice();
 
 		final String script =
 			"(do                                                                   \n" +
-			"	(load-module :xchart)                                              \n" +
+			"   (load-module :xchart)                                              \n" +
 			"                                                                      \n" +
-			"	(xchart/swing-view-chart                                           \n" +
-			"	   (xchart/pie-chart                                               \n" +
-			"	       { \":none\" 845                                             \n" + 
-			"            \":simple\" 371                                           \n" + 
-			"            \":whitespace\" 303                                       \n" +
-			"            \":advanced\" 1013 }                                      \n" +
-			"	       { :title (str \"Which ClojureScript optimization \"         \n" + 
-			"			             \"settings do you use?\")                     \n" + 
-			"            :render-style :donut                                      \n" +
-			"            :annotation-distance 0.82 }))                             \n" +
-			"	                                                                   \n" +
-			"	 (sleep 20000)                                                     \n" +
+			"   (def series1                                                       \n" +
+			"        [ {:x  1 :y  2 :bubble 10}                                    \n" +
+			"          {:x 10 :y  8 :bubble  4}                                    \n" +
+			"          {:x 20 :y 25 :bubble  8} ])                                 \n" +
+			"                                                                      \n" +
+			"   (def series2                                                       \n" +
+			"        [ {:x 10 :y  4 :bubble 10}                                    \n" +
+			"          {:x  5 :y  5 :bubble 12}                                    \n" +
+			"          {:x 18 :y 20 :bubble  3} ])                                 \n" +
+			"                                                                      \n" +			
+			"   (def bubblify                                                      \n" +
+			"      (fn [series]                                                    \n" +
+			"          {:x (map (fn [t] (:x t)) series)                            \n" +
+			"           :y (map (fn [t] (:y t)) series)                            \n" +
+			"           :bubble (map (fn [t] (:bubble t)) series)}))               \n" +
+			"                                                                      \n" +			
+			"   (xchart/swing-view-chart                                           \n" +
+			"      (xchart/bubble-chart*                                           \n" +
+			"          {\"Series 1\" (bubblify series1)                            \n" +
+			"           \"Series 2\" (bubblify series2) }                          \n" +
+			"          {:title \"Bubble Chart\"                                    \n" +
+			"           :legend {:position :inside-ne}                             \n" +
+			"           :y-axis {:title \"Series 1\"}                              \n" +
+			"           :x-axis {:title \"Series 2\" :logarithmic? false}}))       \n" +
+			"                                                                      \n" +
+			"    (sleep 20000)                                                     \n" +
 			") ";
-		
+
 		System.out.println(venice.eval("(str " + script + ")"));
 	}
 
