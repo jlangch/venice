1a145a6c1412c49b4e3d23a6cded74da3bb67d72
juerg <juerg@10.0.1.4>
2018-08-18 14:21:37 +0200

    enhancing try-catch

---
+++ src/main/java/com/github/jlangch/venice/impl/util/CatchBlock.java
@@ -0,0 +1,60 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___ 
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2018 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice.impl.util;
+
+import com.github.jlangch.venice.impl.types.VncSymbol;
+import com.github.jlangch.venice.impl.types.collections.VncList;
+
+public class CatchBlock {
+
+	public CatchBlock(
+			final VncSymbol exSym,
+			final VncList body
+	) {
+		this.exSym = exSym;
+		this.body = body;
+	}
+	
+		
+	public VncSymbol getExSym() {
+		return exSym;
+	}
+	
+	public VncList getBody() {
+		return body;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("SYM: ");
+		sb.append(exSym);
+		sb.append("\n");
+		sb.append(body);
+		
+		return sb.toString();
+	}
+	
+
+	private final VncSymbol exSym;
+	private final VncList body;
+}
