877c65a67926f769079f1a0334dc90b0b8de848f
juerg <juerg@172.20.10.8>
2018-10-20 21:55:16 +0200

    cleanup

--- src/main/java/com/github/jlangch/venice/util/CommandLineArgs.java
+++ src/main/java/com/github/jlangch/venice/util/CommandLineArgs.java
@@ -27,107 +27,105 @@ import java.util.TreeSet;
 
 public class CommandLineArgs {
 
-    public CommandLineArgs(final String[] args){
-        parse(args);
-    }
-
-    public void parse(final String[] arguments){
-        this.args = arguments;
-        switchIndexes.clear();
-        takenIndexes.clear();
-        for(int ii=0; ii < args.length; ii++) {
-            if(args[ii].startsWith("-") ){
-                switchIndexes.put(args[ii], ii);
-                takenIndexes.add(ii);
-            }
-        }
-    }
-
-    public String[] args() {
-        return args;
-    }
-
-    public String arg(final int index){
-        return args[index];
-    }
-
-    public boolean switchPresent(final String switchName) {
-        return switchIndexes.containsKey(switchName);
-    }
-
-    public String switchValue(final String switchName) {
-        return switchValue(switchName, null);
-    }
-
-    public String switchValue(final String switchName, final String defaultValue) {
-        if (!switchIndexes.containsKey(switchName)) {
-        	return defaultValue;
-        }
-
-        int switchIndex = switchIndexes.get(switchName);
-        if (switchIndex + 1 < args.length) {
-            takenIndexes.add(switchIndex +1);
-            return args[switchIndex +1];
-        }
-        return defaultValue;
-    }
-
-    public Long switchLongValue(final String switchName) {
-        return switchLongValue(switchName, null);
-    }
-
-    public Long switchLongValue(final String switchName, final Long defaultValue) {
-        String switchValue = switchValue(switchName, null);
-
-        if(switchValue == null) return defaultValue;
-        return Long.parseLong(switchValue);
-    }
-
-    public Double switchDoubleValue(final String switchName) {
-        return switchDoubleValue(switchName, null);
-    }
-
-    public Double switchDoubleValue(final String switchName, final Double defaultValue) {
-        String switchValue = switchValue(switchName, null);
-
-        if(switchValue == null) return defaultValue;
-        return Double.parseDouble(switchValue);
-    }
-
-
-    public String[] switchValues(final String switchName) {
-        if(!switchIndexes.containsKey(switchName)) return new String[0];
-
-        int switchIndex = switchIndexes.get(switchName);
-
-        int nextArgIndex = switchIndex + 1;
-        while(nextArgIndex < args.length && !args[nextArgIndex].startsWith("-")){
-            takenIndexes.add(nextArgIndex);
-            nextArgIndex++;
-        }
-
-        String[] values = new String[nextArgIndex - switchIndex - 1];
-        for(int jj=0; jj < values.length; jj++){
-            values[jj] = args[switchIndex + jj + 1];
-        }
-        return values;
-    }
-
-    public String[] targets() {
-        String[] targetArray = new String[args.length - takenIndexes.size()];
-        int targetIndex = 0;
-        for(int ii = 0; ii < args.length ; ii++) {
-            if( !takenIndexes.contains(ii) ) {
-                targetArray[targetIndex++] = args[ii];
-            }
-        }
-
-        return targetArray;
-    }
-
-
-    private String[] args = null;
-
-    private HashMap<String, Integer> switchIndexes = new HashMap<String, Integer>();
-    private TreeSet<Integer> takenIndexes  = new TreeSet<Integer>();
+	public CommandLineArgs(final String[] args){
+		parse(args);
+	}
+	
+	public void parse(final String[] arguments){
+		this.args = arguments;
+		switchIndexes.clear();
+		takenIndexes.clear();
+		for(int ii=0; ii < args.length; ii++) {
+			if(args[ii].startsWith("-") ){
+				switchIndexes.put(args[ii], ii);
+				takenIndexes.add(ii);
+			}
+		}
+	}
+	
+	public String[] args() {
+		return args;
+	}
+	
+	public String arg(final int index){
+		return args[index];
+	}
+	
+	public boolean switchPresent(final String switchName) {
+		return switchIndexes.containsKey(switchName);
+	}
+	
+	public String switchValue(final String switchName) {
+		return switchValue(switchName, null);
+	}
+	
+	public String switchValue(final String switchName, final String defaultValue) {
+		if (!switchIndexes.containsKey(switchName)) {
+			return defaultValue;
+		}
+		
+		final int switchIndex = switchIndexes.get(switchName);
+		if (switchIndex + 1 < args.length) {
+			takenIndexes.add(switchIndex +1);
+			return args[switchIndex +1];
+		}
+		return defaultValue;
+	}
+	
+	public Long switchLongValue(final String switchName) {
+		return switchLongValue(switchName, null);
+	}
+	
+	public Long switchLongValue(final String switchName, final Long defaultValue) {
+		final String switchValue = switchValue(switchName, null);
+		
+		return switchValue == null ? defaultValue : Long.parseLong(switchValue);
+	}
+	
+	public Double switchDoubleValue(final String switchName) {
+		return switchDoubleValue(switchName, null);
+	}
+	
+	public Double switchDoubleValue(final String switchName, final Double defaultValue) {
+		String switchValue = switchValue(switchName, null);
+		
+		return switchValue == null ? defaultValue : Double.parseDouble(switchValue);
+	}
+	
+	
+	public String[] switchValues(final String switchName) {
+		if(!switchIndexes.containsKey(switchName)) return new String[0];
+		
+		final int switchIndex = switchIndexes.get(switchName);
+		
+		int nextArgIndex = switchIndex + 1;
+		while(nextArgIndex < args.length && !args[nextArgIndex].startsWith("-")){
+			takenIndexes.add(nextArgIndex);
+			nextArgIndex++;
+		}
+	
+		final String[] values = new String[nextArgIndex - switchIndex - 1];
+		for(int jj=0; jj < values.length; jj++){
+		    values[jj] = args[switchIndex + jj + 1];
+		}
+		return values;
+	}
+	
+	public String[] targets() {
+		final String[] targetArray = new String[args.length - takenIndexes.size()];
+		int targetIndex = 0;
+		for(int ii = 0; ii < args.length ; ii++) {
+			if( !takenIndexes.contains(ii) ) {
+				targetArray[targetIndex++] = args[ii];
+			}
+		}
+	
+		return targetArray;
+	}
+	
+	
+	private String[] args = null;
+	
+	private HashMap<String, Integer> switchIndexes = new HashMap<String, Integer>();
+	private TreeSet<Integer> takenIndexes  = new TreeSet<Integer>();
 }
