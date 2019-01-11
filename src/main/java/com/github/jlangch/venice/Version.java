904282217e140764a20a9bc8ec64ad9677985f6d
juerg <juerg@localhost>
2019-01-11 20:18:42 +0100

    opened new dev cycle

--- src/main/java/com/github/jlangch/venice/Version.java
+++ src/main/java/com/github/jlangch/venice/Version.java
@@ -24,6 +24,6 @@ package com.github.jlangch.venice;
 
 public class Version {
 
-	public static final String VERSION = "1.2.2-snapshot";
+	public static final String VERSION = "1.2.3-snapshot";
 	
 }
