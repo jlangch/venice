e1208b5e423041e8e5e945a2d8613f03997146d3
juerg <juerg@172.20.10.8>
2019-02-22 11:35:48 +0100

    refactoring repl

--- src/main/java/com/github/jlangch/venice/REPL.java
+++ src/main/java/com/github/jlangch/venice/REPL.java
@@ -143,6 +143,8 @@ public class REPL {
 				terminal.flush();
 				write(terminal, "interrupt", " ! interrupted ! ");
 				terminal.flush();
+				terminal.writer().println();
+				terminal.flush();
 				Thread.sleep(1000);
 				break;
 			} 
