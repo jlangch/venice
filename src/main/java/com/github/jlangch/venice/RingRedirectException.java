86af2ef364ab1ce379578f6f6ef6b600aee00997
juerg <juerg@10.0.1.2>
2019-05-04 22:12:46 +0200

    added redirect to ring

---
+++ src/main/java/com/github/jlangch/venice/RingRedirectException.java
@@ -0,0 +1,40 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___ 
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2019 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice;
+
+
+public class RingRedirectException extends VncException {
+
+	public RingRedirectException(final String redirectUri) {
+		super();
+		this.redirectUri = redirectUri;
+	}
+	
+	public String getRedirectUri() {
+		return redirectUri;
+	}
+
+	
+	private static final long serialVersionUID = 5439694361809280080L;
+	
+	private String redirectUri;
+}
