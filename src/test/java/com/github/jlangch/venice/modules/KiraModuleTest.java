f7e6c9ff67f1f5ef5d57e12781622b33a781aba0
juerg <juerg@10.0.1.2>
2019-05-05 18:09:13 +0200

    enhanced kira templates to support custom delimiters

--- src/test/java/com/github/jlangch/venice/modules/KiraModuleTest.java
+++ src/test/java/com/github/jlangch/venice/modules/KiraModuleTest.java
@@ -59,6 +59,34 @@ public class KiraModuleTest {
 	}
 
 	@Test
+	public void test_kira_2_delim_1() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                              \n" +
+				"   (load-module :kira)                           \n" +
+				"                                                 \n" +
+				"   (kira/eval \"<%= 10 %>\" [\"<%\" \"%>\"] {})  \n" + 
+				")";
+
+		assertEquals("10", venice.eval(script));
+	}
+
+	@Test
+	public void test_kira_2_delim_2() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                              \n" +
+				"   (load-module :kira)                           \n" +
+				"                                                 \n" +
+				"   (kira/eval \"$= 10 $\" [\"$\" \"$\"] {})      \n" + 
+				")";
+
+		assertEquals("10", venice.eval(script));
+	}
+
+	@Test
 	public void test_kira_3() {
 		final Venice venice = new Venice();
 
@@ -73,6 +101,34 @@ public class KiraModuleTest {
 	}
 
 	@Test
+	public void test_kira_3_delim_1() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                                       \n" +
+				"   (load-module :kira)                                    \n" +
+				"                                                          \n" +
+				"   (kira/eval \"<%= x %>\" [\"<%\" \"%>\"] {:x \"foo\"})  \n" + 
+				")";
+
+		assertEquals("foo", venice.eval(script));
+	}
+
+	@Test
+	public void test_kira_3_delim_2() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                                       \n" +
+				"   (load-module :kira)                                    \n" +
+				"                                                          \n" +
+				"   (kira/eval \"$= x $\" [\"$\" \"$\"] {:x \"foo\"})      \n" + 
+				")";
+
+		assertEquals("foo", venice.eval(script));
+	}
+
+	@Test
 	public void test_kira_4() {
 		final Venice venice = new Venice();
 
@@ -81,6 +137,34 @@ public class KiraModuleTest {
 				"   (load-module :kira)                   \n" +
 				"                                         \n" +
 				"   (kira/eval \"<%=x%>\" {:x \"foo\"})   \n" + 
+				")";
+
+		assertEquals("foo", venice.eval(script));
+	}
+
+	@Test
+	public void test_kira_4_delim_1() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                                     \n" +
+				"   (load-module :kira)                                  \n" +
+				"                                                        \n" +
+				"   (kira/eval \"<%=x%>\" [\"<%\" \"%>\"] {:x \"foo\"})  \n" + 
+				")";
+
+		assertEquals("foo", venice.eval(script));
+	}
+
+	@Test
+	public void test_kira_4_delim_2() {
+		final Venice venice = new Venice();
+
+		final String script =
+				"(do                                                     \n" +
+				"   (load-module :kira)                                  \n" +
+				"                                                        \n" +
+				"   (kira/eval \"$=x$\" [\"$\" \"$\"] {:x \"foo\"})      \n" + 
 				")";
 
 		assertEquals("foo", venice.eval(script));
@@ -107,6 +191,27 @@ public class KiraModuleTest {
 	}
 
 	@Test
+	public void test_kira_5_delim_2() {
+		final Venice venice = new Venice();
+
+		// (let [xs [1 2 3 4]] (docoll #(print (str "foo" % " ")) xs))
+		
+		// (kira/eval """$ (docoll #(print (str $foo$ % " ")) xs)$""" ["$" "$"] {:xs [1 2 3]})
+		
+		final String script =
+				"(do                                                                  \n" +
+				"   (load-module :kira)                                               \n" +
+				"                                                                     \n" +
+				"   (kira/eval                                                        \n" + 
+				"       \"\"\"$ (docoll #(print (str $foo$ % \" \")) xs)$\"\"\"       \n" + 
+				"      [\"$\" \"$\"]                                                  \n" + 
+				"      {:xs [1 2 3]})                                                 \n" + 
+				")";
+
+		assertEquals("foo1 foo2 foo3 ", venice.eval(script));
+	}
+
+	@Test
 	public void test_kira_6() {
 		final Venice venice = new Venice();
 
@@ -117,6 +222,22 @@ public class KiraModuleTest {
 				"   (load-module :kira)                                               \n" +
 				"                                                                     \n" +
 				"   (let [tf (kira/fn [x] \"foo<%= x %>\")] (tf \"bar\"))               " +
+				")";
+
+		assertEquals("foobar", venice.eval(script));
+	}
+
+	@Test
+	public void test_kira_6_delim_2() {
+		final Venice venice = new Venice();
+
+		// (let [func (kira/fn [x] "foo$= x $" ["$" "$"])] (func "bar"))
+		
+		final String script =
+				"(do                                                                  \n" +
+				"   (load-module :kira)                                               \n" +
+				"                                                                     \n" +
+				"   (let [tf (kira/fn [x] \"foo$= x $\" [\"$\" \"$\"])] (tf \"bar\"))  " +
 				")";
 
 		assertEquals("foobar", venice.eval(script));
