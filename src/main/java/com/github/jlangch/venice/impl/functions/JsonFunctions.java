bf4a503f522088a9ca25d5e72bac7e2512be511f
juerg <juerg@10.0.1.2>
2019-05-12 00:09:09 +0200

    Json writer supports now date/time, byte buffers, and decimals by emitting formatted strings

--- src/main/java/com/github/jlangch/venice/impl/functions/JsonFunctions.java
+++ src/main/java/com/github/jlangch/venice/impl/functions/JsonFunctions.java
@@ -29,23 +29,42 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.Reader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import com.github.jlangch.venice.VncException;
 import com.github.jlangch.venice.impl.types.Constants;
+import com.github.jlangch.venice.impl.types.VncBigDecimal;
+import com.github.jlangch.venice.impl.types.VncByteBuffer;
 import com.github.jlangch.venice.impl.types.VncDouble;
 import com.github.jlangch.venice.impl.types.VncFunction;
+import com.github.jlangch.venice.impl.types.VncInteger;
+import com.github.jlangch.venice.impl.types.VncJavaObject;
 import com.github.jlangch.venice.impl.types.VncKeyword;
 import com.github.jlangch.venice.impl.types.VncLong;
 import com.github.jlangch.venice.impl.types.VncString;
+import com.github.jlangch.venice.impl.types.VncSymbol;
 import com.github.jlangch.venice.impl.types.VncVal;
 import com.github.jlangch.venice.impl.types.collections.VncHashMap;
+import com.github.jlangch.venice.impl.types.collections.VncJavaList;
+import com.github.jlangch.venice.impl.types.collections.VncJavaMap;
+import com.github.jlangch.venice.impl.types.collections.VncJavaSet;
 import com.github.jlangch.venice.impl.types.collections.VncList;
+import com.github.jlangch.venice.impl.types.collections.VncMap;
+import com.github.jlangch.venice.impl.types.collections.VncSet;
+import com.github.jlangch.venice.impl.types.collections.VncVector;
 import com.github.jlangch.venice.impl.types.util.Coerce;
 import com.github.jlangch.venice.impl.types.util.Types;
+import com.grack.nanojson.JsonAppendableWriter;
 import com.grack.nanojson.JsonParser;
 import com.grack.nanojson.JsonWriter;
 
@@ -81,14 +100,23 @@ public class JsonFunctions {
 					return Nil;
 				}
 				else {
-					final Object javaVal = val.convertToJavaObject();
 					
 					final VncHashMap options = VncHashMap.ofAll(args.slice(1));				
 					final VncVal pretty = options.get(new VncKeyword("pretty")); 
 
-					return pretty == Constants.True
-							? new VncString(JsonWriter.indent(INDENT).string().value(javaVal).done())
-							: new VncString(JsonWriter.string(javaVal));
+					final StringBuilder sb = new StringBuilder();
+					
+					final JsonAppendableWriter writer = pretty == Constants.True
+															? JsonWriter.indent(INDENT).on(sb)
+															: JsonWriter.on(sb);
+								
+					write(writer, val);
+					writer.done();
+					
+					// final Object javaVal = val.convertToJavaObject();
+					// writer.value(javaVal).done();
+					
+					return new VncString(sb.toString());
 				}
 			}
 	
@@ -130,20 +158,16 @@ public class JsonFunctions {
 					final VncVal pretty = options.get(new VncKeyword("pretty")); 
 
 					if (out instanceof OutputStream) {
-						if (pretty == Constants.True) {
-							JsonWriter.indent(INDENT).on((OutputStream)out).value(javaVal).done();
-						}
-						else {
-							JsonWriter.on((OutputStream)out).value(javaVal).done();
-						}
+						final JsonAppendableWriter writer = pretty == Constants.True
+																? JsonWriter.indent(INDENT).on((OutputStream)out)
+																: JsonWriter.on((OutputStream)out);
+						writer.value(javaVal).done();
 					}
 					else if (out instanceof PrintStream) {
-						if (pretty == Constants.True) {
-							JsonWriter.indent(INDENT).on((PrintStream)out).value(javaVal).done();
-						}
-						else {
-							JsonWriter.on((PrintStream)out).value(javaVal).done();
-						}
+						final JsonAppendableWriter writer = pretty == Constants.True
+																? JsonWriter.indent(INDENT).on((PrintStream)out)
+																: JsonWriter.on((PrintStream)out);
+						writer.value(javaVal).done();
 					}
 					else {
 						throw new VncException(String.format(
@@ -231,8 +255,6 @@ public class JsonFunctions {
 									"Function 'json/slurp' does not allow %s as in",
 									Types.getType(args.first())));
 						}
-
-						
 					}
 					catch(Exception ex) {
 						throw new VncException("Failed to parse JSON", ex);
@@ -321,8 +343,238 @@ public class JsonFunctions {
 		throw new VncException("Failed to parse JSON. Unsupported Java type: " + value.getClass());
 	}
 
-	
 	private static final String INDENT = "  ";
+	
+	
+	private static void write(final JsonAppendableWriter writer, final VncVal val) {
+		if (Types.isVncConstant(val)) {
+			if (val == Constants.Nil) {
+				writer.nul();
+			}
+			else if (val == Constants.True) {
+				writer.value(true);
+			}
+			else if (val == Constants.False) {
+				writer.value(false);
+			}
+		}
+		else if (Types.isVncString(val)) {
+			writer.value(((VncString)val).getValue());
+		}
+		else if (Types.isVncInteger(val)) {
+			writer.value(((VncInteger)val).getValue().intValue());
+		}
+		else if (Types.isVncLong(val)) {
+			writer.value(((VncLong)val).getValue().longValue());
+		}
+		else if (Types.isVncDouble(val)) {
+			writer.value(((VncDouble)val).getValue().doubleValue());
+		}
+		else if (Types.isVncBigDecimal(val)) {
+			writer.value(((VncBigDecimal)val).getValue().toString());
+		}
+		else if (Types.isVncKeyword(val)) {
+			writer.value(((VncKeyword)val).getValue());
+		}
+		else if (Types.isVncSymbol(val)) {
+			writer.value(((VncSymbol)val).getName());
+		}
+		else if (Types.isVncList(val)) {
+			writer.array();
+			((VncList)val).forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncVector(val)) {
+			writer.array();
+			((VncVector)val).forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncJavaList(val)) {
+			writer.array(((List<?>)((VncJavaList)val).getDelegate()));
+		}
+		else if (Types.isVncJavaObject(val)) {
+			final Object delegate = ((VncJavaObject)val).getDelegate();
+			if (delegate instanceof LocalDate) {
+				writer.value(((LocalDate)delegate).format(FMT_LOCAL_DATE));
+			}
+			else if (delegate instanceof LocalDateTime) {
+				writer.value(((LocalDateTime)delegate).format(FMT_LOCAL_DATE_TIME));
+			}
+			else if (delegate instanceof ZonedDateTime) {
+				writer.value(((ZonedDateTime)delegate).format(FMT_DATE_TIME));
+			}
+			else {
+				throw new VncException(String.format(
+						"Json serialization error: the type %s can not be serialized",
+						Types.getType(val)));
+			}
+		}
+		else if (Types.isVncJavaMap(val)) {
+			writer.object(((Map<?,?>)((VncJavaMap)val).getDelegate()));
+		}
+		else if (Types.isVncMap(val)) {
+			writer.object();
+			final Map<VncVal,VncVal> map = ((VncMap)val).getMap();
+			for(Entry<VncVal,VncVal> e : map.entrySet()) {
+				final VncVal k = e.getKey();
+				final VncVal v = e.getValue();
+				if (Types.isVncString(k)) {
+					write(writer, ((VncString)k).getValue(), v);
+				}
+				else if (Types.isVncKeyword(k)) {
+					write(writer, ((VncKeyword)k).getValue(), v);
+				}
+				else if (Types.isVncLong(k)) {
+					write(writer, ((VncLong)k).getValue().toString(), v);
+				}
+				else {
+					throw new VncException(String.format(
+							"Json serialization error: the map key type %s can not be serialized",
+							Types.getType(val)));
+				}
+			}
+			writer.end();
+		}
+		else if (Types.isVncSet(val)) {
+			writer.array();
+			((VncSet)val).getSet().forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncJavaSet(val)) {
+			writer.array(((Set<?>)((VncJavaSet)val).getDelegate()));
+		}
+		else if (Types.isVncSet(val)) {
+			writer.array();
+			((VncSet)val).getSet().forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncByteBuffer(val)) {
+			final byte[] buf = ((VncByteBuffer)val).getValue().array();
+			writer.value(Base64.getEncoder().encodeToString(buf));
+		}
+		else {
+			throw new VncException(String.format(
+					"Json serialization error: the type %s can not be serialized",
+					Types.getType(val)));
+		}
+	}
+
+	private static void write(final JsonAppendableWriter writer, final String key, final VncVal val) {
+		if (Types.isVncConstant(val)) {
+			if (val == Constants.Nil) {
+				writer.nul(key);
+			}
+			else if (val == Constants.True) {
+				writer.value(key, true);
+			}
+			else if (val == Constants.False) {
+				writer.value(key, false);
+			}
+		}
+		else if (Types.isVncString(val)) {
+			writer.value(key, ((VncString)val).getValue());
+		}
+		else if (Types.isVncInteger(val)) {
+			writer.value(key, ((VncInteger)val).getValue().intValue());
+		}
+		else if (Types.isVncLong(val)) {
+			writer.value(key, ((VncLong)val).getValue().longValue());
+		}
+		else if (Types.isVncDouble(val)) {
+			writer.value(key, ((VncDouble)val).getValue().doubleValue());
+		}
+		else if (Types.isVncBigDecimal(val)) {
+			writer.value(key, ((VncBigDecimal)val).getValue().toString());
+		}
+		else if (Types.isVncKeyword(val)) {
+			writer.value(key, ((VncKeyword)val).getValue());
+		}
+		else if (Types.isVncSymbol(val)) {
+			writer.value(key, ((VncSymbol)val).getName());
+		}
+		else if (Types.isVncList(val)) {
+			writer.array(key);
+			((VncList)val).forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncVector(val)) {
+			writer.array(key);
+			((VncVector)val).forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncJavaList(val)) {
+			writer.array(key, ((List<?>)((VncJavaList)val).getDelegate()));
+		}
+		else if (Types.isVncJavaObject(val)) {
+			final Object delegate = ((VncJavaObject)val).getDelegate();
+			if (delegate instanceof LocalDate) {
+				writer.value(key, ((LocalDate)delegate).format(FMT_LOCAL_DATE));
+			}
+			else if (delegate instanceof LocalDateTime) {
+				writer.value(key, ((LocalDateTime)delegate).format(FMT_LOCAL_DATE_TIME));
+			}
+			else if (delegate instanceof ZonedDateTime) {
+				writer.value(key, ((ZonedDateTime)delegate).format(FMT_DATE_TIME));
+			}
+			else {
+				throw new VncException(String.format(
+						"Json serialization error: the type %s can not be serialized",
+						Types.getType(val)));
+			}
+		}
+		else if (Types.isVncJavaMap(val)) {
+			writer.object(key, ((Map<?,?>)((VncJavaMap)val).getDelegate()));
+		}
+		else if (Types.isVncMap(val)) {
+			writer.object(key);
+			final Map<VncVal,VncVal> map = ((VncMap)val).getMap();
+			for(Entry<VncVal,VncVal> e : map.entrySet()) {
+				final VncVal k = e.getKey();
+				final VncVal v = e.getValue();
+				if (Types.isVncString(k)) {
+					write(writer, ((VncString)k).getValue(), v);
+				}
+				else if (Types.isVncKeyword(k)) {
+					write(writer, ((VncKeyword)k).getValue(), v);
+				}
+				else if (Types.isVncLong(k)) {
+					write(writer, ((VncLong)k).getValue().toString(), v);
+				}
+				else {
+					throw new VncException(String.format(
+							"Json serialization error: the map key type %s can not be serialized",
+							Types.getType(val)));
+				}
+			}
+			writer.end();
+		}
+		else if (Types.isVncSet(val)) {
+			writer.array(key);
+			((VncSet)val).getSet().forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncJavaSet(val)) {
+			writer.array(key, ((Set<?>)((VncJavaSet)val).getDelegate()));
+		}
+		else if (Types.isVncSet(val)) {
+			writer.array(key);
+			((VncSet)val).getSet().forEach(v -> write(writer, v));
+			writer.end();
+		}
+		else if (Types.isVncByteBuffer(val)) {
+			final byte[] buf = ((VncByteBuffer)val).getValue().array();
+			writer.value(key, Base64.getEncoder().encodeToString(buf));
+		}
+		else {
+			throw new VncException(String.format(
+					"Json serialization error: the type %s can not be serialized",
+					Types.getType(val)));
+		}
+	}
+
+	private static final DateTimeFormatter FMT_LOCAL_DATE = DateTimeFormatter.ISO_LOCAL_DATE;
+	private static final DateTimeFormatter FMT_LOCAL_DATE_TIME = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+	private static final DateTimeFormatter FMT_DATE_TIME = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 	
 	
 	///////////////////////////////////////////////////////////////////////////
