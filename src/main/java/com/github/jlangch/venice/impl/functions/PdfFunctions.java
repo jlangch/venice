35c4417157135c90464524d0e308e66c7be9be36
juerg <juerg@localhost>
2019-07-03 17:06:27 +0200

    worked on pdf watermarks

--- src/main/java/com/github/jlangch/venice/impl/functions/PdfFunctions.java
+++ src/main/java/com/github/jlangch/venice/impl/functions/PdfFunctions.java
@@ -22,18 +22,29 @@
 package com.github.jlangch.venice.impl.functions;
 
 import static com.github.jlangch.venice.impl.functions.FunctionsUtil.assertArity;
+import static com.github.jlangch.venice.impl.functions.FunctionsUtil.assertMinArity;
 
+import java.awt.Color;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.github.jlangch.venice.impl.types.Constants;
 import com.github.jlangch.venice.impl.types.VncByteBuffer;
+import com.github.jlangch.venice.impl.types.VncDouble;
 import com.github.jlangch.venice.impl.types.VncFunction;
+import com.github.jlangch.venice.impl.types.VncKeyword;
+import com.github.jlangch.venice.impl.types.VncLong;
+import com.github.jlangch.venice.impl.types.VncString;
 import com.github.jlangch.venice.impl.types.VncVal;
 import com.github.jlangch.venice.impl.types.collections.VncHashMap;
 import com.github.jlangch.venice.impl.types.collections.VncList;
 import com.github.jlangch.venice.impl.types.util.Coerce;
+import com.github.jlangch.venice.impl.util.StringUtil;
+import com.github.jlangch.venice.pdf.HtmlColor;
 import com.github.jlangch.venice.pdf.PdfRenderer;
+import com.github.jlangch.venice.pdf.PdfWatermark;
 
 
 public class PdfFunctions {
@@ -88,6 +99,76 @@ public class PdfFunctions {
 	
 		    private static final long serialVersionUID = -1848883965231344442L;
 		};
+
+	public static VncFunction pdf_watermark = 
+		new VncFunction(
+				"pdf/watermark", 
+				VncFunction
+					.meta()
+					.module("pdf")
+					.arglists("(pdf/watermark pdf text & options)")		
+					.doc(
+						"Adds a watermark text to the PDF pages.\n\n" +
+						"Options: \n" +
+						"  :font-size n         - font size (double), defaults to 24.0 \n" +
+						"  :font-char-spacing n - font character spacing (double), defaults to 0.0 \n" +
+						"  :color c             - font color (HTML color string), defaults to #000000 \n" +
+						"  :opacity n           - opacity 0.0 ... 1.0 (double), defaults to 0.8 \n" +
+						"  :angle n             - angle 0.0 ... 360.0 (double), defaults to 45.0 \n" +
+						"  :over-content b      - print text over the content (boolean), defaults to true \n" +
+						"  :skip-top-pages n    - the number of top pages to skip (long), defaults to 0 \n" +
+						"  :skip-bottom-pages n - the number of bottom pages to skip (long), defaults to 0")
+					.build()
+		) {		
+			public VncVal apply(final VncList args) {
+				assertMinArity("pdf/watermark", args, 2);
+				
+				final VncVal pdf = args.first();
+				final VncVal text = args.second();
+
+				final VncHashMap options = VncHashMap.ofAll(args.slice(2));
+
+				final VncVal fontSize = options.get(new VncKeyword("font-size", new VncDouble(24.0))); 
+				final VncVal fontCharSpacing = options.get(new VncKeyword("font-char-spacing", new VncDouble(0.0))); 
+				final VncVal color = options.get(new VncKeyword("color", new VncString("#000000"))); 
+				final VncVal opacity = options.get(new VncKeyword("opacity", new VncDouble(0.8))); 
+				final VncVal angle = options.get(new VncKeyword("angle", new VncDouble(45.0))); 
+				final VncVal overContent = options.get(new VncKeyword("over-content", Constants.True)); 
+				final VncVal skipTopPages = options.get(new VncKeyword("skip-top-pages", new VncLong(0))); 
+				final VncVal skipBottomPages = options.get(new VncKeyword("skip-bottom-pages", new VncLong(0))); 
+
+				final ByteBuffer pdf_ = Coerce.toVncByteBuffer(pdf).getValue();
+				final String text_ = Coerce.toVncString(text).getValue();
+				final float fontSize_ = Coerce.toVncDouble(fontSize).getValue().floatValue();
+				final float fontCharSpacing_ = Coerce.toVncDouble(fontCharSpacing).getValue().floatValue();
+				final Color color_ = HtmlColor.getColor(Coerce.toVncString(color).getValue());
+				final float opacity_= Coerce.toVncDouble(opacity).getValue().floatValue();
+				final float angle_= Coerce.toVncDouble(angle).getValue().floatValue();
+				final boolean overContent_ = Coerce.toVncBoolean(overContent) == Constants.True ? true : false;
+				final int skipTopPages_ = Coerce.toVncLong(skipTopPages).getValue().intValue();
+				final int skipBottomPages_ = Coerce.toVncLong(skipBottomPages).getValue().intValue();
+
+				if (StringUtil.isBlank(text_)) {
+					return pdf;
+				}
+
+				return new VncByteBuffer(
+						new PdfWatermark()
+								.addWatermarkText(
+									pdf_, 
+									text_,
+									fontSize_,
+									fontCharSpacing_,
+									color_,
+									opacity_,
+									angle_,
+									overContent_,
+									skipTopPages_, 
+									skipBottomPages_));
+			}
+	
+		    private static final long serialVersionUID = -1848883965231344442L;
+		};
 	
 		
 		
@@ -98,6 +179,7 @@ public class PdfFunctions {
 	public static Map<VncVal, VncVal> ns = 
 			new VncHashMap.Builder()
 					.put("pdf/render", pdf_render)
+					.put("pdf/watermark", pdf_watermark)
 					.toMap();	
 	
 }
