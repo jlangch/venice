b80b87994d03c4fce5105b1d751af74d68cb45e3
juerg <juerg@10.0.1.5>
2019-07-31 11:22:17 +0200

    fixed comment typo

--- src/main/java/com/github/jlangch/venice/impl/VeniceInterpreter.java
+++ src/main/java/com/github/jlangch/venice/impl/VeniceInterpreter.java
@@ -630,7 +630,7 @@ public class VeniceInterpreter implements Serializable  {
 			final VncVal fn = env.getGlobalOrNull((VncSymbol)a0);
 			if (!Types.isVncMacro(fn)) break;
 			
-			// validate that the macro allowed by the sandox
+			// validate that the macro allowed by the sandbox
 			interceptor.validateVeniceFunction(((VncFunction)fn).getName());					
 
 			expanded = true;
