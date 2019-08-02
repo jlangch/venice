e9fd6d3451cd3f13bc09e435a3c95036fa16850f
juerg <juerg@localhost>
2019-08-02 15:42:21 +0200

    formatting

--- src/main/java/com/github/jlangch/venice/impl/functions/IOFnBlacklisted.java
+++ src/main/java/com/github/jlangch/venice/impl/functions/IOFnBlacklisted.java
@@ -29,84 +29,85 @@ import java.util.Set;
 public class IOFnBlacklisted {
 
 	public static Set<String> getAllIoFunctions() {
-		return new HashSet<>(Arrays.asList(
-								// load (macros)
-								"load-file",
-								"load-classpath-file",
+		return new HashSet<>(
+				Arrays.asList(
+					// load (macros)
+					"load-file",
+					"load-classpath-file",
 
-								// system
-								"gc",
-								"shutdown-hook",
-								"sleep",
-								"sh",
-								"system-prop",
-								
-								// concurrency
-								"deliver",
-								"future",
-								"future?",
-								"future-cancel",
-								"future-cancelled?",
-								"future-done?",
-								"promise",
-								"promise?",
-								"agent",
-								"send",
-								"send-off",
-								"restart-agent",
-								"set-error-handler!",
-								"agent-error",
-								"agent-error-mode",
-								"await",
-								"await-for",
-								"shutdown-agents",
-								"shutdown-agents?",
-								"await-termination-agents",
-								"await-termination-agents?",
-								
-								// scheduler
-								"schedule-delay",
-								"schedule-at-fixed-rate",
+					// system
+					"gc",
+					"shutdown-hook",
+					"sleep",
+					"sh",
+					"system-prop",
+					
+					// concurrency
+					"deliver",
+					"future",
+					"future?",
+					"future-cancel",
+					"future-cancelled?",
+					"future-done?",
+					"promise",
+					"promise?",
+					"agent",
+					"send",
+					"send-off",
+					"restart-agent",
+					"set-error-handler!",
+					"agent-error",
+					"agent-error-mode",
+					"await",
+					"await-for",
+					"shutdown-agents",
+					"shutdown-agents?",
+					"await-termination-agents",
+					"await-termination-agents?",
+					
+					// scheduler
+					"schedule-delay",
+					"schedule-at-fixed-rate",
 
-								// I/O
-								"io/copy-file",
-								"io/copy-stream",
-								"io/delete-file",
-								"io/delete-file-on-exit",
-								"io/download",
-								"io/exists-dir?",
-								"io/exists-file?",
-								"io/file-size",
-								"io/list-files",
-								"io/move-file",
-								"io/mkdir",
-								"io/mkdirs",
-								"io/slurp",
-								"io/slurp-stream",
-								"io/slurp-lines",
-								"io/spit",
-								"io/spit-stream",
-								"io/temp-file",
-								"io/tmp-dir",
-								"io/user-dir",
-								"io/load-classpath-resource",
-								
-								// I/O zip
-								"io/zip",
-								"io/zip-append",
-								"io/zip-remove",
-								"io/zip-file",
-								"io/zip-list",
-								"io/unzip",
-								"io/unzip-first",
-								"io/unzip-nth",
-								"io/unzip-all",
-								"io/unzip-to-dir",
-								"io/zip-size",
-								"io/gzip",
-								"io/gzip-to-stream",
-								"io/ungzip",
-								"io/ungzip-to-stream"));
+					// I/O
+					"io/copy-file",
+					"io/copy-stream",
+					"io/delete-file",
+					"io/delete-file-on-exit",
+					"io/download",
+					"io/exists-dir?",
+					"io/exists-file?",
+					"io/file-size",
+					"io/list-files",
+					"io/move-file",
+					"io/mkdir",
+					"io/mkdirs",
+					"io/slurp",
+					"io/slurp-stream",
+					"io/slurp-lines",
+					"io/spit",
+					"io/spit-stream",
+					"io/temp-file",
+					"io/tmp-dir",
+					"io/user-dir",
+					"io/load-classpath-resource",
+					
+					// I/O zip
+					"io/zip",
+					"io/zip-append",
+					"io/zip-remove",
+					"io/zip-file",
+					"io/zip-list",
+					"io/unzip",
+					"io/unzip-first",
+					"io/unzip-nth",
+					"io/unzip-all",
+					"io/unzip-to-dir",
+					"io/zip-size",
+					"io/gzip",
+					"io/gzip-to-stream",
+					"io/ungzip",
+					"io/ungzip-to-stream"));
 	}
 
 }
