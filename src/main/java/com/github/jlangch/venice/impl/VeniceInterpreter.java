48e5c330102038c6e72f377b7deed8a1d3d7ea6d
jlangch <juerg.lang@ggaweb.ch>
2019-11-24 18:30:24 +0100

    worked on walk module

--- src/main/java/com/github/jlangch/venice/impl/VeniceInterpreter.java
+++ src/main/java/com/github/jlangch/venice/impl/VeniceInterpreter.java
@@ -427,6 +427,13 @@ public class VeniceInterpreter implements Serializable  {
 						return macroexpand(ast.second(), env);
 					}
 					
+				case "macroexpand-sym": 
+					try (WithCallStack cs = new WithCallStack(CallFrame.fromVal("macroexpand-sym", ast))) {
+						final VncVal sym = ast.second();
+						final VncVal val = Types.isVncSymbol(sym) ? env.get((VncSymbol)sym) : sym;
+						return macroexpand(val, env);
+					}
+					
 				case "quote":
 					return ast.second();
 					
