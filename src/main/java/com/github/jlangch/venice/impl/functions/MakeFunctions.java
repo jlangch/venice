761baa1f618ef1df7ea04c31749d3f2543218c9f
juerg <juerg@localhost>
2019-12-12 18:49:28 +0100

    refactored function make-app

--- src/main/java/com/github/jlangch/venice/impl/functions/MakeFunctions.java
+++ src/main/java/com/github/jlangch/venice/impl/functions/MakeFunctions.java
@@ -34,11 +34,13 @@ import java.util.Map;
 import com.github.jlangch.venice.Version;
 import com.github.jlangch.venice.VncException;
 import com.github.jlangch.venice.impl.types.VncFunction;
+import com.github.jlangch.venice.impl.types.VncString;
 import com.github.jlangch.venice.impl.types.VncVal;
 import com.github.jlangch.venice.impl.types.collections.VncHashMap;
 import com.github.jlangch.venice.impl.types.collections.VncList;
 import com.github.jlangch.venice.impl.types.collections.VncMap;
 import com.github.jlangch.venice.impl.types.collections.VncMapEntry;
+import com.github.jlangch.venice.impl.types.collections.VncOrderedMap;
 import com.github.jlangch.venice.impl.types.util.Coerce;
 import com.github.jlangch.venice.impl.types.util.Types;
 import com.github.jlangch.venice.impl.util.Zipper;
@@ -51,7 +53,7 @@ public class MakeFunctions {
 				"make-app", 
 				VncFunction
 					.meta()
-					.arglists("(make-app name main-file file-map dest-file)")		
+					.arglists("(make-app name main-file file-map dest-dir)")		
 					.doc("Creates a Venice application archive.")
 					.build()
 		) {	
@@ -59,32 +61,23 @@ public class MakeFunctions {
 				assertArity("make-app", args, 4);
 
 				try {
-					final String name = Coerce.toVncString(args.first()).getValue();
+					final VncString name = Coerce.toVncString(args.first());
 
-					final String mainFileName = Coerce.toVncString(args.second()).getValue();
+					final VncString mainFileName = Coerce.toVncString(args.second());
 
 					final VncMap files = Coerce.toVncMap(args.third());
 					
-					final File destFile = IOFunctions.convertToFile(
+					final File destDir = IOFunctions.convertToFile(
 											args.fourth(),
-											"Function 'make-app' dest-file is not a file");
+											"Function 'make-app' dest-dir is not a file");
+
+					validateDestDirExists(destDir);
 					
 					final Map<String, Object> zipEntries = new LinkedHashMap<>();
 					
-					final String manifest = String.format(
-												"{ \n" +
-												"   \"manifest-version\" : \"%s\", \n" +
-												"   \"app-name\" : \"%s\", \n" +
-												"   \"main-file\" : \"%s\" \n" +
-												"   \"created-by\" : \"%s %s\" \n" +
-												"}",
-												"1.0",
-												name,
-												mainFileName,
-												"Venice",
-												Version.VERSION);
+					final VncString manifest = buildManifest(name, mainFileName);
 											
-					zipEntries.put("MANIFEST.MF", manifest.getBytes("utf-8"));
+					zipEntries.put("MANIFEST.MF", manifest.getValue().getBytes("utf-8"));
 									
 					for(VncMapEntry e : files.entries()) {
 						final String path = Coerce.toVncString(e.getKey()).getValue();
@@ -94,8 +87,12 @@ public class MakeFunctions {
 														"Function 'make-app' not a file %s -> %s",
 														path,
 														Types.getType(e.getValue())));
-						
-						zipEntries.put(path, Files.readAllBytes(file.toPath()));
+
+						validateSourceFileExists(file);
+
+						zipEntries.put(
+							makeVeniceFileName(path), 
+							Files.readAllBytes(file.toPath()));
 					}
 					
 					final OpenOption[] openOptions = new OpenOption[] {
@@ -105,7 +102,7 @@ public class MakeFunctions {
 
 					final byte[] data = Zipper.zip(zipEntries);
 					
-					Files.write(destFile.toPath(), data, openOptions);
+					Files.write(new File(destDir, name.getValue() + ".zip").getAbsoluteFile().toPath(), data, openOptions);
 					
 					return Nil;
 				} 
@@ -117,6 +114,45 @@ public class MakeFunctions {
 		    private static final long serialVersionUID = -1848883965231344442L;
 		};
 		
+		
+	private static VncString buildManifest(
+		final VncString name,
+		final VncString mainFileName
+	) {
+		return Coerce.toVncString(
+				JsonFunctions.write_str.apply(
+				  VncList.of(
+					VncOrderedMap.of(
+						new VncString("manifest-version"), new VncString("1.0"),
+						new VncString("app-name"), name,
+						new VncString("main-file"), mainFileName),
+						new VncString("created-by"), new VncString("Venice " + Version.VERSION))));
+	}
+	
+	private static String makeVeniceFileName(final String path) {
+		if (path.endsWith(".venice")) {
+			return path;
+		}
+		else {
+			return path + ".venice";
+		}
+	}
+	
+	private static void validateDestDirExists(final File dir) {
+		if (!dir.isDirectory()) {
+			throw new VncException(String.format(
+					"Function 'make-app' dest-dir '%s' does not exist.",
+					dir.getPath()));
+		}
+	}
+	
+	private static void validateSourceFileExists(final File file) {
+		if (!file.isFile()) {
+			throw new VncException(String.format(
+					"Function 'make-app' file '%s' does not exist.",
+					file.getPath()));
+		}
+	}
 	
 		
 	///////////////////////////////////////////////////////////////////////////
