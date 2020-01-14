a0180200c395fc4b6125c34280670d1cb1cdd853
juerg <juerg@localhost>
2020-01-14 12:40:46 +0100

    reorganized imports

--- src/main/java/com/github/jlangch/venice/javainterop/RejectAllInterceptor.java
+++ src/main/java/com/github/jlangch/venice/javainterop/RejectAllInterceptor.java
@@ -26,7 +26,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import com.github.jlangch.venice.impl.ModuleLoader;
 import com.github.jlangch.venice.impl.functions.IOFnBlacklisted;
 
 
