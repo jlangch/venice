8b670abb6215e800d92aca0077f4cc9283c7131a
juerg <juerg@localhost>
2020-03-14 19:32:32 +0100

    renamed test class

--- src/test/java/com/github/jlangch/venice/MultiMethodTest.java
+++ src/test/java/com/github/jlangch/venice/MultiMethodTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 
-public class MultiFnTest {
+public class MultiMethodTest {
 
 	@Test
 	public void test_defmulti_simple() {
