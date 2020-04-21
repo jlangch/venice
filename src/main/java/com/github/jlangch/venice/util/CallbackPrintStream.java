0e77aa25134e7876d892514885d1ac26ea8e5f75
juerg <juerg@localhost>
2020-04-21 21:39:23 +0200

    added CallbackPrintStreamTest

---
+++ src/main/java/com/github/jlangch/venice/util/CallbackPrintStream.java
@@ -0,0 +1,187 @@
+/*   __	__		 _
+ *   \ \  / /__ _ __ (_) ___ ___ 
+ *	\ \/ / _ \ '_ \| |/ __/ _ \
+ *	 \  /  __/ | | | | (_|  __/
+ *	  \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2020 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice.util;
+
+import java.io.PrintStream;
+import java.util.function.Consumer;
+
+
+public class CallbackPrintStream extends PrintStream {
+
+	public CallbackPrintStream(
+			final boolean autoFlush,
+			final Consumer<String> printer
+	) {
+		super(new NullOutputStream());
+		
+		this.autoFlush = autoFlush;
+		this.printer = printer;
+	}
+
+	
+	@Override
+	public PrintStream append(final CharSequence csq) {
+		print(csq == null ? "null" : csq.toString());
+		return this;
+	}
+	
+	@Override
+	public PrintStream append(final CharSequence csq, final int start, final int end) {
+		CharSequence cs = (csq == null ? "null" : csq);
+		print(cs.subSequence(start, end).toString());
+		return this;
+	}
+
+	@Override
+	public PrintStream append(final char c) {
+		print(c);
+		return this;
+	}
+
+	@Override
+	public void print(final boolean x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final int x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final long x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final float x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final double x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final char x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final char[] x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final Object x) {
+		print(String.valueOf(x));
+	}
+
+	@Override
+	public void print(final String s) {
+		synchronized (this) {
+			sb.append(s);
+			
+			if (autoFlush && (s.indexOf('\n') >= 0)) {
+				flush();
+			}
+		}
+	}
+
+	@Override
+	public void println(final boolean x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final int x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final long x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final float x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final double x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final char x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final char[] x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final Object x) {
+		println(String.valueOf(x));
+	}
+
+	@Override
+	public void println(final String s) {
+		synchronized (this) {
+			sb.append(s).append("\n");		
+			flush();
+		}
+	}
+
+	@Override
+	public void write(final byte buf[], final int off, final int len) {
+		throw new RuntimeException(
+				"Method write(byte[],int,int) is not supported");
+	}
+	
+	@Override
+	public void write(final int b) {
+		throw new RuntimeException(
+				"Method write(int) is not supported");
+	}
+	
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public void flush() {
+		synchronized (this) {
+			printer.accept(sb.toString());
+			sb.setLength(0);
+		}
+	}
+
+	
+	private final Consumer<String> printer;
+	private final StringBuilder sb = new StringBuilder();
+	private final boolean autoFlush;
+}
---
+++ src/test/java/com/github/jlangch/venice/util/CallbackPrintStreamTest.java
@@ -0,0 +1,47 @@
+package com.github.jlangch.venice.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CallbackPrintStreamTest {
+
+	@Test
+	public void test() throws Exception {
+		final PrintStream orgStdOut = System.out;
+		final List<String> captured = new ArrayList<>();
+		
+		try {
+	        System.setOut(new CallbackPrintStream(true, s -> captured.add(s)));
+	        
+	        System.out.println(100);
+	        System.out.println("abc");
+	        System.out.println("d\ne");
+	        System.out.print(300);
+	        System.out.print('-');
+	        System.out.print(400);
+	        System.out.flush();
+	        System.out.println(500.0);
+	        
+			assertEquals(5, captured.size());
+			assertEquals("100\n", captured.get(0));
+			assertEquals("abc\n", captured.get(1));
+			assertEquals("d\ne\n", captured.get(2));
+			assertEquals("300-400", captured.get(3));
+			assertEquals("500.0\n", captured.get(4));
+	        
+			System.setOut(orgStdOut);
+		}
+		catch(Exception ex) {
+			System.setOut(orgStdOut);
+			throw ex;
+		}
+	}
+
+	
+}
