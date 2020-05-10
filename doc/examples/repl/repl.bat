74091bab90e8febf837b8ad1661a49739b1a84a2
juerg <juerg@localhost>
2020-05-10 17:40:06 +0200

    added some examples to venice jar

---
+++ doc/examples/repl/repl.bat
@@ -0,0 +1,24 @@
+REM # -------------------------------------------------------------------------
+REM # Starts a Venice REPL
+REM # -------------------------------------------------------------------------
+REM # --home
+REM #    |
+REM #    +-- libs
+REM #    |    +-- venice-1.7.25.jar
+REM #    |    +-- jansi-1.18.jar
+REM #    |
+REM #    +-- repl.json
+REM #    |
+REM #    +-- repl.bat
+REM # -------------------------------------------------------------------------
+
+cd C:/Users/foo/venice
+
+java.exe ^
+  -server ^
+  -Xmx2G ^
+  -XX:-OmitStackTraceInFastThrow ^
+  -cp "libs\*" ^
+  com.github.jlangch.venice.Launcher ^
+  -loadpath "scripts" 
+  -colors
--- doc/examples/repl/repl.sh
+++ doc/examples/repl/repl.sh
@@ -1,11 +1,29 @@
+#!/bin/sh
+# ------------------------------------------------------------------------------
+# Starts a Venice REPL
+# ------------------------------------------------------------------------------
+# --home
+#    |
+#    +-- libs
+#    |    +-- venice-1.7.25.jar
+#    |
+#    +-- scripts
+#    |    +-- script-1.venice
+#    |    +-- script-2.venice
+#    |
+#    +-- repl.json
+#    |
+#    +-- repl.sh
+# ------------------------------------------------------------------------------
 
-cd /Users/juerg/Desktop/venice/
 
-${JAVA_11_HOME}/bin/java \
+cd /Users/foo/venice/
+
+java \
   -server \
-  -Xmx2G \
+  -Xmx4G \
   -XX:-OmitStackTraceInFastThrow \
   -cp "libs/*" \
   com.github.jlangch.venice.Launcher \
-  -loadpath /Users/juerg/Desktop/venice/scripts \
+  -loadpath "scripts" \
   -colors
