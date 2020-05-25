e43576fb7ade1f6ce3dd225391947b196397229f
juerg <juerg@localhost>
2020-05-25 14:08:26 +0200

    added parsatron unit tests

---
+++ src/test/java/com/github/jlangch/venice/modules/ParsatronModuleTest.java
@@ -0,0 +1,313 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___ 
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2020 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice.modules;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.jlangch.venice.Venice;
+import com.github.jlangch.venice.VncException;
+
+
+public class ParsatronModuleTest {
+
+	@Test
+	public void test_basic() {
+		final String script =
+				"(do                                               \n" +
+				"   (load-module :parsatron)                       \n" +
+				"   (parsatron/run (parsatron/char (char \"H\"))   \n" +
+				"                  \"Hello, world!\"))              ";
+
+		assertEquals("H", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_basic_input() {
+		final String script =
+				"(do                                               \n" +
+				"   (load-module :parsatron)                       \n" +
+				"   (parsatron/run (parsatron/token #{1 2})        \n" +
+				"                  [1 \"cats\" :dogs]))              ";
+
+		assertEquals(1L, (Long)new Venice().eval(script));
+	}
+
+	@Test
+	public void test_basic_error() {
+		final String script =
+				"(do                                               \n" +
+				"   (load-module :parsatron)                       \n" +
+				"   (parsatron/run (parsatron/char (char \"Q\"))   \n" +
+				"                  \"Hello, world!\"))              ";
+		
+		assertThrows(VncException.class, () -> new Venice().eval(script));
+	}
+
+	@Test
+	public void test_basic_parser() {
+		final String script =
+				"(do                                               \n" +
+				"   (load-module :parsatron)                       \n" +
+				"   (def h-parser (parsatron/char (char \"H\")))   \n" +
+				"   (parsatron/run h-parser                        \n" +
+				"                  \"Hello, world!\"))              ";
+
+		assertEquals("H", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_token() {
+		final String script =
+				"(do                                                 \n" +
+				"   (load-module :parsatron)                         \n" +
+				"   (defn less-than-five [i] (< i 5))                \n" +
+				"   (parsatron/run (parsatron/token less-than-five)  \n" +
+				"                  [3]))                               ";
+
+		assertEquals(3L, (Long)new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_token_number() {
+		final String script =
+				"(do                                                 \n" +
+				"   (load-module :parsatron)                         \n" +
+				"   (parsatron/run (parsatron/token number?)         \n" +
+				"                  [10 20 30]))                        ";
+
+		assertEquals(10L, (Long)new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_char() {
+		final String script =
+				"(do                                                 \n" +
+				"   (load-module :parsatron)                         \n" +
+				"   (parsatron/run (parsatron/char (char \"H\"))     \n" +
+				"                  \"Hello, world!\"))                 ";
+
+		assertEquals("H", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_anychar() {
+		final String script1 =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/any-char)  \n" +
+				"                  \"Cats\"))               ";
+
+		assertEquals("C", new Venice().eval(script1));
+
+		
+		final String script2 =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/any-char)  \n" +
+				"                  (seq \"Cats\")))        ";
+
+		assertEquals("C", new Venice().eval(script2));
+
+		
+		final String script3 =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/any-char)  \n" +
+				"                  [1 2 3]))               ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script3));
+	}
+
+	@Test
+	public void test_builtin_parser_letter() {
+		final String script =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/letter)    \n" +
+				"                  \"Dogs\"))              ";
+
+		assertEquals("D", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_digit() {
+		final String script =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/digit)     \n" +
+				"                  \"1234\"))              ";
+
+		assertEquals("1", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_string() {
+		final String script =
+				"(do                                                \n" +
+				"   (load-module :parsatron)                        \n" +
+				"   (parsatron/run (parsatron/string \"Hello\")     \n" +
+				"                  \"Hello, world!\"))                ";
+
+		assertEquals("Hello", new Venice().eval(script));
+	}
+
+	@Test
+	public void test_builtin_parser_eof() {
+		final String script1 =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/eof)       \n" +
+				"                  \"\"))                 ";
+
+		assertNull(new Venice().eval(script1));
+
+	
+		final String script2 =
+				"(do                                     \n" +
+				"   (load-module :parsatron)             \n" +
+				"   (parsatron/run (parsatron/eof)       \n" +
+				"                  \"a\"))                 ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script2));
+	}
+
+	@Test
+	public void test_builtin_parser_composition() {
+		final String script1 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/char (char \"a\"))    \n" +
+				"                                (parsatron/digit)))              \n" +
+				"   (parsatron/run my-parser \"a5\"))                               ";
+
+		assertEquals("5", new Venice().eval(script1));
+
+	
+		final String script2 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/char (char \"a\"))    \n" +
+				"                                (parsatron/digit)))              \n" +
+				"   (parsatron/run my-parser \"5a\"))                               ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script2));
+
+		
+		final String script3 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/char (char \"a\"))    \n" +
+				"                                (parsatron/digit)))              \n" +
+				"   (parsatron/run my-parser \"b5\"))                               ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script3));
+
+		
+		final String script4 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/char (char \"a\"))    \n" +
+				"                                (parsatron/digit)))              \n" +
+				"   (parsatron/run my-parser \"aq\"))                               ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script4));
+
+		
+		final String script5 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/digit)                \n" +
+				"                                (parsatron/eof)))                \n" +
+				"   (parsatron/run my-parser \"1\"))                                ";
+
+		assertNull(new Venice().eval(script5));
+
+		
+		final String script6 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/digit)                \n" +
+				"                                (parsatron/eof)))                \n" +
+				"   (parsatron/run my-parser \"1 cat\"))                            ";
+
+		assertThrows(VncException.class, () -> new Venice().eval(script6));
+	}
+
+	@Test
+	public void test_builtin_parser_times() {
+		final String script1 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (pr-str                                                       \n" +
+				"     (parsatron/run (parsatron/times 5 (parsatron/letter))       \n" +
+				"                    \"Hello, world!\")))                           ";
+
+		assertEquals("[\"H\" \"e\" \"l\" \"l\" \"o\"]", new Venice().eval(script1));
+
+
+		final String script2 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (def my-parser (parsatron/>> (parsatron/letter)               \n" +
+				"                                (parsatron/letter)               \n" +
+				"                                (parsatron/letter)               \n" +
+				"                                (parsatron/letter)               \n" +
+				"                                (parsatron/letter)))             \n" +
+				"   (parsatron/run my-parser \"Hello, world!\"))                    ";
+
+		assertEquals("o", new Venice().eval(script2));
+	}
+
+	@Test
+	public void test_builtin_parser_many() {
+		final String script1 =
+				"(do                                                              \n" +
+				"   (load-module :parsatron)                                      \n" +
+				"   (pr-str                                                       \n" +
+				"     (parsatron/run (parsatron/many (parsatron/digit))           \n" +
+				"                    \"100 cats\")))                                ";
+
+		assertEquals("[\"1\" \"0\" \"0\"]", new Venice().eval(script1));
+
+	
+		final String script2 =
+				"(do                                                                     \n" +
+				"   (load-module :parsatron)                                             \n" +
+				"                                                                        \n" +
+				"   (def number-parser (parsatron/many (parsatron/digit)))               \n" +
+				"   (def whitespace-parser (parsatron/many                               \n" +
+				"                              (parsatron/token str/whitespace?)))       \n" +
+				"                                                                        \n" +
+				"   (pr-str                                                              \n" +
+				"     (parsatron/run (parsatron/>> number-parser                         \n" +
+				"                                  whitespace-parser                     \n" +
+				"                                  number-parser)                        \n" +
+				"                    \"100  400\")))                                       ";
+
+		assertEquals("[\"4\" \"0\" \"0\"]", new Venice().eval(script2));
+	}
+
+}
