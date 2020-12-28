bc26f0fdfe6f099e8a9ba1b62723019d22b26262
Juerg Lang <juerg.lang@ggaweb.ch>
2020-12-28 10:20:37 +0100

    customized javadoc

---
+++ src/main/java/com/github/jlangch/venice/examples/package-info.java
@@ -0,0 +1,27 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___ 
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2021 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Classes in this package are examples and are not intended to be accessed 
+ * directly. Therefore, they are not included in the JavaDocs.
+ */
+package com.github.jlangch.venice.examples;
