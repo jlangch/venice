fb9bbe71e483ac9a52a0668e6dbf2654b8e4a338
Juerg Lang <juerg.lang@ggaweb.ch>
2021-08-28 19:43:29 +0200

    fixed unit test package

--- src/test/java/com/github/jlangch/venice/impl/types/thread/ThreadContextSnapshotTest.java
+++ src/test/java/com/github/jlangch/venice/impl/types/thread/ThreadContextSnapshotTest.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jlangch.venice.impl.types.concurrent;
+package com.github.jlangch.venice.impl.types.thread;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
