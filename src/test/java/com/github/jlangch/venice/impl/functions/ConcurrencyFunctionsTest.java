3f1ea4a6ebb1eaba24eab74453c21a75e7fb931c
Juerg Lang <juerg.lang@ggaweb.ch>
2021-08-28 20:25:32 +0200

    increased a timout time in an agent unit test to comply with slow Github CI test environment

--- src/test/java/com/github/jlangch/venice/impl/functions/ConcurrencyFunctionsTest.java
+++ src/test/java/com/github/jlangch/venice/impl/functions/ConcurrencyFunctionsTest.java
@@ -579,7 +579,7 @@ public class ConcurrencyFunctionsTest {
 				"         (send relay relay-fn 0 msg)))                                      \n" +
 				"                                                                            \n" +
 				"   (process (create-relay 5) \"hello\")                                     \n" +
-				"   (sleep 800)                                                              \n" +
+				"   (sleep 1200)                                                              \n" +
 				"   (with-out-str (print @logger)))                                            ";
 
 		assertEquals(
