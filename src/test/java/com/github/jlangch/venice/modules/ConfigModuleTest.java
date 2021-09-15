785d900a2f06e784d9c7b7a7aa44ce1ed16e5d37
jlangch <juerg.lang@ggaweb.ch>
2021-09-15 22:24:53 +0200

    updated unit tests

--- src/test/java/com/github/jlangch/venice/modules/ConfigModuleTest.java
+++ src/test/java/com/github/jlangch/venice/modules/ConfigModuleTest.java
@@ -312,19 +312,20 @@ public class ConfigModuleTest {
 		final Venice venice = new Venice();
 
 		final String script =
-				"(do                                                               \n" +
-				"  (load-module :config)                                           \n" +
-				"                                                                  \n" +
-				"  (defn config []                                                 \n" + 
-				"    (config/build                                                 \n" + 
-				"      (json/read-str \"\"\"{\"app\": {\"pwd\": \"123\"}}\"\"\"    \n" +
-				"                     :key-fn keyword)                             \n" +
-				"      (config/env-var \"M_SERVER_PORT\"                           \n" +
-				"                      [:http :port]                               \n" +
-				"                      \"8000\")))                                 \n" +
-				"                                                                  \n" +
-				"  (pr-str [ (-> (config) :app :pwd)                               \n" +
-				"            (-> (config) :http :port)]))                            ";
+				"(do                                                       \n" +
+				"  (load-module :config)                                   \n" +
+				"                                                          \n" +
+				"  (def app-cfg \"\"\"{\"app\": {\"pwd\": \"123\"}}\"\"\") \n" +
+				"                                                          \n" +
+				"  (defn config []                                         \n" + 
+				"    (config/build                                         \n" + 
+				"      (json/read-str app-cfg :key-fn keyword)             \n" +
+				"      (config/env-var \"M_SERVER_PORT\"                   \n" +
+				"                      [:http :port]                       \n" +
+				"                      \"8000\")))                         \n" +
+				"                                                          \n" +
+				"  (pr-str [ (-> (config) :app :pwd)                       \n" +
+				"            (-> (config) :http :port)]))                    ";
 
 		assertEquals("[\"123\" \"8000\"]", venice.eval(script));
 	}
