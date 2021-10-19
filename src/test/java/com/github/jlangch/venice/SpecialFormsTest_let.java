eebd933fd571af02e1084e5b1c76985db3ece285
Juerg Lang <juerg.lang@ggaweb.ch>
2021-10-19 21:53:30 +0200

    added unit tests

--- src/test/java/com/github/jlangch/venice/SpecialFormsTest_let.java
+++ src/test/java/com/github/jlangch/venice/SpecialFormsTest_let.java
@@ -35,8 +35,44 @@ public class SpecialFormsTest_let {
 		assertEquals(10L, venice.eval("(let [x 10] x)"));
 		assertEquals(21L, venice.eval("(let [x 10 y 11] (+ x y))"));
 		assertEquals(60L, venice.eval("(let [x 10 y (* x 5)] (+ x y))"));
+	}
+	
+	@Test
+	public void test_let_destructure() {
+		final Venice venice = new Venice();
 
 		assertEquals(21L, venice.eval("(let [[x y] [10 11]] (+ x y))"));
 		assertEquals(71L, venice.eval("(let [[x y] [10 11] z (* x 5)] (+ x y z))"));
+	}
+	
+	@Test
+	public void test_let_9() {
+		final Venice venice = new Venice();
+
+		assertEquals(
+			"[10 1 11]", 
+			venice.eval(
+					"(do                           \n" +
+					"  (defn sum [x y]             \n" +
+					"    (let [u 10]               \n" +
+					"      (let [y 11]             \n" +
+					"        [u x y])))            \n" +
+					"  (pr-str (sum 1 2)))"));
+	}
+	
+	@Test
+	public void test_let_10() {
+		final Venice venice = new Venice();
+
+		assertEquals(
+			"[100 11 1 2]", 
+			venice.eval(
+					"(do                             \n" +
+					"  (let [a 100]                  \n" +
+					"    (defn sum [x y]             \n" +
+					"      (let [u 10]               \n" +
+					"        (let [u 11]             \n" +
+					"          [a u x y]))))         \n" +
+					"  (pr-str (sum 1 2)))"));
 	}
 }
