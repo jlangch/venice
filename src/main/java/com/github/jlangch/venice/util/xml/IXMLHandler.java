53a6be9a38e56b7b7df21486b02e70e8ee4a31f5
Juerg Lang <juerg.lang@ggaweb.ch>
2022-06-14 15:32:11 +0200

    refactored packages

--- src/main/java/com/github/jlangch/venice/util/xml/IXMLHandler.java
+++ src/main/java/com/github/jlangch/venice/util/xml/IXMLHandler.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jlangch.venice.util;
+package com.github.jlangch.venice.util.xml;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.Locator;
--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,10 +1,14 @@
 module com.github.jlangch.venice {
 
     exports com.github.jlangch.venice;
+    exports com.github.jlangch.venice.demo;
     exports com.github.jlangch.venice.javainterop;
     exports com.github.jlangch.venice.jsr223;
-    exports com.github.jlangch.venice.pdf;
-    exports com.github.jlangch.venice.servlet;
     exports com.github.jlangch.venice.util;
+    exports com.github.jlangch.venice.util.excel;
+    exports com.github.jlangch.venice.util.pdf;
+    exports com.github.jlangch.venice.util.servlet;
+    exports com.github.jlangch.venice.util.word;
+    exports com.github.jlangch.venice.util.xml;
 
 }
