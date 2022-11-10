097c14f80b56ab8a1b92c411156364e5ee31f57e
Juerg Lang <juerg.lang@ggaweb.ch>
2022-11-10 19:02:40 +0100

    refactoring cheatsheet source package

--- src/main/java/com/github/jlangch/venice/impl/docgen/sourcecode/SourceCodeRenderer.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/sourcecode/SourceCodeRenderer.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jlangch.venice.impl.docgen.source;
+package com.github.jlangch.venice.impl.docgen.sourcecode;
 
 import java.nio.ByteBuffer;
 import java.util.Map;
