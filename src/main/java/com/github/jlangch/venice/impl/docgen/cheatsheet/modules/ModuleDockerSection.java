8d0d3fe24a1e7401a8da083f66e30ae172fdae69
Juerg Lang <juerg.lang@ggaweb.ch>
2023-08-31 20:49:22 +0200

    improved :docker module documentation

--- src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleDockerSection.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleDockerSection.java
@@ -79,6 +79,7 @@ public class ModuleDockerSection implements ISectionBuilder {
         utils.addItem(diBuilder.getDocItem("docker/container-start-by-name", false));
         utils.addItem(diBuilder.getDocItem("docker/container-stop-by-name", false));
         utils.addItem(diBuilder.getDocItem("docker/container-exec-by-name", false));
+        utils.addItem(diBuilder.getDocItem("docker/container-status-by-name", false));
 
         return section;
     }
