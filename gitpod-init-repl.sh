79a2a5d2f377b3be1c6ecec72f0e69deda9793b0
Juerg Lang <juerg.lang@ggaweb.ch>
2023-09-27 19:45:55 +0200

    added copyright header to gitpod shell scripts

--- gitpod-init-repl.sh
+++ gitpod-init-repl.sh
@@ -1,4 +1,25 @@
 # -----------------------------------------------------------------------------
+#   __    __         _
+#   \ \  / /__ _ __ (_) ___ ___
+#    \ \/ / _ \ '_ \| |/ __/ _ \
+#     \  /  __/ | | | | (_|  __/
+#      \/ \___|_| |_|_|\___\___|
+#
+#
+# Copyright 2017-2023 Venice
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------------
 # Initializes the REPL for a new Gitpod instance
 #
 # This script is run as part of the .gitpod.yaml config
