11d715d43d4fd327f7664ebe815ef3844deec033
jlangch <juerg.lang@ggaweb.ch>
2023-10-18 22:34:01 +0200

    improved :jsonl examples

--- src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleJsonlSection.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleJsonlSection.java
@@ -46,8 +46,8 @@ public class ModuleJsonlSection implements ISectionBuilder {
         all.addSection(docker);
         docker.addItem(diBuilder.getDocItem("jsonl/write-str", true));
         docker.addItem(diBuilder.getDocItem("jsonl/read-str", true));
-        docker.addItem(diBuilder.getDocItem("jsonl/spit", false));
-        docker.addItem(diBuilder.getDocItem("jsonl/slurp", false));
+        docker.addItem(diBuilder.getDocItem("jsonl/spit", true));
+        docker.addItem(diBuilder.getDocItem("jsonl/slurp", true));
 
         return section;
     }
