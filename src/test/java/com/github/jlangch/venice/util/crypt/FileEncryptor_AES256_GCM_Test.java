188b4aa8d9418f4175a4c83eda3b6cb3f202ee92
juerg <juerg@pluto.fritz.box>
2023-12-11 12:33:21 +0100

    added junit tests

---
+++ src/test/java/com/github/jlangch/venice/util/crypt/FileEncryptor_AES256_GCM_Test.java
@@ -0,0 +1,61 @@
+package com.github.jlangch.venice.util.crypt;
+
+import static com.github.jlangch.venice.util.crypt.FileEncryptor_AES256_GCM.decryptFileWithKey;
+import static com.github.jlangch.venice.util.crypt.FileEncryptor_AES256_GCM.decryptFileWithPassphrase;
+import static com.github.jlangch.venice.util.crypt.FileEncryptor_AES256_GCM.encryptFileWithKey;
+import static com.github.jlangch.venice.util.crypt.FileEncryptor_AES256_GCM.encryptFileWithPassphrase;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.security.SecureRandom;
+
+import org.junit.jupiter.api.Test;
+
+
+public class FileEncryptor_AES256_GCM_Test {
+
+	@Test
+    public void testPassphrase() throws Exception {
+
+        final byte[] data = "1234567890".getBytes("UTF-8");
+        final byte[] encrypted = encryptFileWithPassphrase(data, "passphrase");
+        final byte[] decrypted = decryptFileWithPassphrase(encrypted, "passphrase");
+
+        if (data.length != decrypted.length) {
+        	fail("FAIL (length)");
+        	return;
+        }
+
+        for(int ii=0; ii<data.length; ii++) {
+        	if (data[ii] != decrypted[ii]) {
+            	fail("FAIL (@index " + ii + ")");
+            	return;
+        	}
+        }
+    }
+
+
+	@Test
+    public void testKey() throws Exception {
+	    byte[] key = new byte[32];
+	    new SecureRandom().nextBytes(key);
+
+        final byte[] data = "1234567890".getBytes("UTF-8");
+        final byte[] encrypted = encryptFileWithKey(data, key);
+        final byte[] decrypted = decryptFileWithKey(encrypted, key);
+
+        if (data.length != decrypted.length) {
+        	fail("FAIL (length)");
+        	return;
+        }
+
+        for(int ii=0; ii<data.length; ii++) {
+        	if (data[ii] != decrypted[ii]) {
+            	fail("FAIL (@index " + ii + ")");
+            	return;
+        	}
+        }
+    }
+
+
+
+}
