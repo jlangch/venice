4d3a04637107b4b1c1f39ff26e08da3f9bd2a6bb
juerg <juerg@localhost>
2023-12-19 14:29:02 +0100

    added :matrix unit tests

---
+++ src/test/java/com/github/jlangch/venice/modules/MatrixModuleTest.java
@@ -0,0 +1,394 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2022 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice.modules;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.jlangch.venice.Venice;
+import com.github.jlangch.venice.VncException;
+
+
+public class MatrixModuleTest {
+
+	// ------------------------------------------------------------------------
+	// validate
+	// ------------------------------------------------------------------------
+
+    @Test
+    public void test_validate_ok_1() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate []))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_2() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                         \n" +
+                "   (load-module :matrix)    \n" +
+                "   (matrix/validate [[]]))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_3() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                           \n" +
+                "   (load-module :matrix)       \n" +
+                "   (matrix/validate [[] []]))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_4() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                          \n" +
+                "   (load-module :matrix)     \n" +
+                "   (matrix/validate [[1]]))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_5() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                            \n" +
+                "   (load-module :matrix)       \n" +
+                "   (matrix/validate [[1 2]]))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_6() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                              \n" +
+                "   (load-module :matrix)         \n" +
+                "   (matrix/validate [[1] [1]]))  ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_ok_7() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                                 \n" +
+                "   (load-module :matrix)            \n" +
+                "   (matrix/validate [[1 2] [1 2]])) ";
+
+        venice.eval(script);
+    }
+
+    @Test
+    public void test_validate_fail_1() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate nil))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+    @Test
+    public void test_validate_fail_2() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate 2))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+    @Test
+    public void test_validate_fail_3() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate [5]))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+    @Test
+    public void test_validate_fail_4() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate [[] 4))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+    @Test
+    public void test_validate_fail_5() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate [[5] 4))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+    @Test
+    public void test_validate_fail_6() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/validate [[5] [4 5]))  ";
+
+        assertThrows(VncException.class, () -> venice.eval(script));
+    }
+
+
+	// ------------------------------------------------------------------------
+	// empty?
+	// ------------------------------------------------------------------------
+
+    @Test
+    public void test_empty_1() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/empty? []))  ";
+
+        assertTrue((Boolean)venice.eval(script));
+    }
+
+    @Test
+    public void test_empty_2() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/empty? [[]]))  ";
+
+        assertFalse((Boolean)venice.eval(script));
+    }
+
+    @Test
+    public void test_empty_3() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/empty? [[] []]))  ";
+
+        assertFalse((Boolean)venice.eval(script));
+    }
+
+    @Test
+    public void test_empty_4() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/empty? [[4]]))  ";
+
+        assertFalse((Boolean)venice.eval(script));
+    }
+
+    @Test
+    public void test_empty_5() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/empty? [[4] [5]]))  ";
+
+        assertFalse((Boolean)venice.eval(script));
+    }
+
+
+
+	// ------------------------------------------------------------------------
+	// rows
+	// ------------------------------------------------------------------------
+
+    @Test
+    public void test_rows_1() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/rows []))      ";
+
+        assertEquals(0L, venice.eval(script));
+    }
+
+    @Test
+    public void test_rows_2() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/rows [[1]]))      ";
+
+        assertEquals(1L, venice.eval(script));
+    }
+
+    @Test
+    public void test_rows_3() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/rows [[1 2]]))      ";
+
+        assertEquals(1L, venice.eval(script));
+    }
+
+    @Test
+    public void test_rows_4() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/rows [[1] [2]]))      ";
+
+        assertEquals(2L, venice.eval(script));
+    }
+
+    @Test
+    public void test_rows_5() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/rows [[1 2] [1 2]]))      ";
+
+        assertEquals(2L, venice.eval(script));
+    }
+
+
+
+	// ------------------------------------------------------------------------
+	// columns
+	// ------------------------------------------------------------------------
+
+    @Test
+    public void test_columns_1() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/columns []))   ";
+
+        assertEquals(0L, venice.eval(script));
+    }
+
+    @Test
+    public void test_columns_2() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                       \n" +
+                "   (load-module :matrix)  \n" +
+                "   (matrix/columns [[1]])) ";
+
+        assertEquals(1L, venice.eval(script));
+    }
+
+    @Test
+    public void test_columns_3() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                           \n" +
+                "   (load-module :matrix)      \n" +
+                "   (matrix/columns [[1 2]]))  ";
+
+        assertEquals(2L, venice.eval(script));
+    }
+
+    @Test
+    public void test_columns_4() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                            \n" +
+                "   (load-module :matrix)       \n" +
+                "   (matrix/columns [[1] [2]])) ";
+
+        assertEquals(1L, venice.eval(script));
+    }
+
+    @Test
+    public void test_columns_5() {
+        final Venice venice = new Venice();
+
+        final String script =
+                "(do                                 \n" +
+                "   (load-module :matrix)            \n" +
+                "   (matrix/columns [[1 2] [1 2]]))  ";
+
+        assertEquals(2L, venice.eval(script));
+    }
+
+  }
