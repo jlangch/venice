2176845a9083ad94461e3efbadc62bad158f538f
juerg <juerg@pluto.fritz.box>
2024-03-10 17:13:41 +0100

    bumped to new version

--- src/main/resources/com/github/jlangch/venice/repl.bat
+++ src/main/resources/com/github/jlangch/venice/repl.bat
@@ -6,7 +6,7 @@ REM # --home
 REM #    |
 REM #    +-- libs
 REM #    |    +-- repl.json
-REM #    |    +-- venice-1.12.3.jar
+REM #    |    +-- venice-1.12.4.jar
 REM #    |
 REM #    +-- scripts
 REM #    |    +-- script-1.venice
--- src/main/resources/com/github/jlangch/venice/repl.sh
+++ src/main/resources/com/github/jlangch/venice/repl.sh
@@ -6,7 +6,7 @@
 #    |
 #    +-- libs
 #    |    +-- repl.json
-#    |    +-- venice-1.12.3.jar
+#    |    +-- venice-1.12.4.jar
 #    |
 #    +-- scripts
 #    |    +-- script-1.venice
