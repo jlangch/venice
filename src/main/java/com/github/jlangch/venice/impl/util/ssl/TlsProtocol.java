c6e9723c3e349a459dfa555d81da802df9baa4a0
jlangch <juerg.lang@ggaweb.ch>
2024-04-03 17:07:20 +0200

    Added SSLSocketFactoryUtil

---
+++ src/main/java/com/github/jlangch/venice/impl/util/ssl/TlsProtocol.java
@@ -0,0 +1,46 @@
+/*   __    __         _
+ *   \ \  / /__ _ __ (_) ___ ___
+ *    \ \/ / _ \ '_ \| |/ __/ _ \
+ *     \  /  __/ | | | | (_|  __/
+ *      \/ \___|_| |_|_|\___\___|
+ *
+ *
+ * Copyright 2017-2024 Venice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jlangch.venice.impl.util.ssl;
+
+
+public enum TlsProtocol {
+
+    TLS_1_0("TLSv1.0"),
+
+    TLS_1_1("TLSv1.1"),
+
+    TLS_1_2("TLSv1.2"),
+
+    TLS_1_3("TLSv1.3");
+
+
+    @Override
+    public String toString() {
+        return version;
+    }
+
+    private TlsProtocol(final String version) {
+        this.version = version;
+    }
+
+    private final String version;
+}
