3ab6cfda367713716808ce20cfe3960394ffe5e7
jlangch <juerg.lang@ggaweb.ch>
2024-05-08 07:53:17 +0200

    added function images/basic-stroke

--- src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleImagesSection.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleImagesSection.java
@@ -73,6 +73,7 @@ public class ModuleImagesSection implements ISectionBuilder {
         all.addSection(drawing);
         drawing.addItem(diBuilder.getDocItem("images/g2d", false));
         drawing.addItem(diBuilder.getDocItem("images/anti-alias", false));
+        drawing.addItem(diBuilder.getDocItem("images/basic-stroke", false));
         drawing.addItem(diBuilder.getDocItem("images/fill-circle", false));
         drawing.addItem(diBuilder.getDocItem("images/fill-oval", false));
         drawing.addItem(diBuilder.getDocItem("images/fill-rect", false));
