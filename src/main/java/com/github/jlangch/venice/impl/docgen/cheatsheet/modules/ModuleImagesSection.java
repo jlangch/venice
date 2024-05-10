78cc30326f0fffffc23ec4e09e29c60cdadd7636
jlangch <juerg.lang@ggaweb.ch>
2024-05-10 18:58:41 +0200

    worked om images functions

--- src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleImagesSection.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleImagesSection.java
@@ -82,6 +82,17 @@ public class ModuleImagesSection implements ISectionBuilder {
         g2d.addItem(diBuilder.getDocItem("images/set-clip", false));
         g2d.addItem(diBuilder.getDocItem("images/get-clip-bounds", false));
 
+        final DocSection tx = new DocSection("Transform", id());
+        all.addSection(tx);
+        tx.addItem(diBuilder.getDocItem("images/set-transform", false));
+        tx.addItem(diBuilder.getDocItem("images/get-transform", false));
+        tx.addItem(diBuilder.getDocItem("images/transform", false));
+        tx.addItem(diBuilder.getDocItem("images/tx-identity", false));
+        tx.addItem(diBuilder.getDocItem("images/tx-translate", false));
+        tx.addItem(diBuilder.getDocItem("images/tx-scale", false));
+        tx.addItem(diBuilder.getDocItem("images/tx-shear", false));
+        tx.addItem(diBuilder.getDocItem("images/tx-rotate", false));
+
         final DocSection drawing = new DocSection("Drawing", id());
         all.addSection(drawing);
         drawing.addItem(diBuilder.getDocItem("images/copy-area", false));
@@ -108,6 +119,15 @@ public class ModuleImagesSection implements ISectionBuilder {
         shapes.addItem(diBuilder.getDocItem("images/point", false));
         shapes.addItem(diBuilder.getDocItem("images/rectangle", false));
         shapes.addItem(diBuilder.getDocItem("images/polygon", false));
+
+        final DocSection poly = new DocSection("Polygons", id());
+        all.addSection(poly);
+        poly.addItem(diBuilder.getDocItem("images/hexagon-poly", false));
+        poly.addItem(diBuilder.getDocItem("images/rectangle-poly", false));
+        poly.addItem(diBuilder.getDocItem("images/square-poly", false));
+        poly.addItem(diBuilder.getDocItem("images/scale-points", false));
+        poly.addItem(diBuilder.getDocItem("images/translate-points", false));
+        poly.addItem(diBuilder.getDocItem("images/rotate-points", false));
 
         return section;
     }
