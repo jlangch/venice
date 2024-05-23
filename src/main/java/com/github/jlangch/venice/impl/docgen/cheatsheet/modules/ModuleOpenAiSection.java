0c72a247ccf2f50c9afb1507c46507eb4f2e3c49
jlangch <juerg.lang@ggaweb.ch>
2024-05-23 21:20:13 +0200

    added openai assistant-delete

--- src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleOpenAiSection.java
+++ src/main/java/com/github/jlangch/venice/impl/docgen/cheatsheet/modules/ModuleOpenAiSection.java
@@ -94,6 +94,7 @@ public class ModuleOpenAiSection implements ISectionBuilder {
         all.addSection(assistants);
         assistants.addItem(diBuilder.getDocItem("openai/assistant-create", false));
         assistants.addItem(diBuilder.getDocItem("openai/assistant-list", false));
+        assistants.addItem(diBuilder.getDocItem("openai/assistant-delete", false));
 
         final DocSection utils = new DocSection("Utils", id());
         all.addSection(utils);
