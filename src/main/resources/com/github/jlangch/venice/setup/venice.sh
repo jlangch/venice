e6da2bf065f7961ecd182801e5f5f80a6990410e
jlangch <juerg.lang@ggaweb.ch>
2024-12-04 19:10:51 +0100

    updated venice devel scripts

--- src/main/resources/com/github/jlangch/venice/setup/venice.sh
+++ src/main/resources/com/github/jlangch/venice/setup/venice.sh
@@ -1,10 +1,10 @@
 ###############################################################################
 # Venice shell                                                                #
 # ----------------------------------------------------------------------------#
-# Starts a Venice shell (custom REPL), loads 'venice.venice' and runs it.     #
+# Starts a Venice shell (REPL), loads 'venice.venice' and runs it.            #
 #                                                                             #
 # Layout:                                                                     #
-#    VENICE_SHELL_HOME                                                        #
+#    REPL_HOME                                                                #
 #      +--libs                                                                #
 #      |   +-- venice-x.y.z.jar                                               #
 #      |   +-- jansi-2.4.1.jar                                                #
@@ -15,6 +15,8 @@
 export VENICE_SHELL_HOME=/Users/juerg/Desktop/scripts
 export VENICE_PROJECT_HOME=/Users/juerg/Documents/workspace-omni/venice
 export VENICE_REPL_HOME=/Users/juerg/Desktop/venice
+
+export VENICE_JAVA_HOME=${JAVA_8_HOME}
 
 
 if [ ! -d ${VENICE_SHELL_HOME} ]; then
@@ -33,9 +35,9 @@ if [ ! -d ${VENICE_SHELL_HOME}/libs ]; then
   exit 1
 fi
 
-if [ ! -d ${JAVA_8_HOME} ]; then
+if [ ! -d ${VENICE_JAVA_HOME} ]; then
   echo
-  echo "Error: The Java 8 home dir ${JAVA_8_HOME} does not exist!"
+  echo "Error: The Java home dir ${VENICE_JAVA_HOME} does not exist!"
   echo
   read -p "Press any key to exit..." -n 1 -s
   exit 1
@@ -45,7 +47,7 @@ fi
 cd ${VENICE_SHELL_HOME}
 
 
-${JAVA_8_HOME}/bin/java \
+${VENICE_JAVA_HOME}/bin/java \
   -server \
   -cp "libs:libs/*" com.github.jlangch.venice.Launcher \
   -Xmx2G \
