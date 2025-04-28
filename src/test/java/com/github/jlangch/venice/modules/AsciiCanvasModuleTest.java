ba29d05244a95f6f29b31cbb07e775104338af0f
jlangch <juerg.lang@ggaweb.ch>
2025-04-28 09:11:23 +0200

    added unit tests

--- src/test/java/com/github/jlangch/venice/modules/AsciiCanvasModuleTest.java
+++ src/test/java/com/github/jlangch/venice/modules/AsciiCanvasModuleTest.java
@@ -76,4 +76,189 @@ public class AsciiCanvasModuleTest {
                      "|        |\n" +
                      "+--------+", venice.eval(script));
     }
+
+    @Test
+    public void test_003_box() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/box 0 0 10 5 \"┌┐┘└─│─│\")                \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("┌────────┐\n" +
+                     "│        │\n" +
+                     "│        │\n" +
+                     "│        │\n" +
+                     "└────────┘", venice.eval(script));
+    }
+
+    @Test
+    public void test_004_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-text \"Hello\" 0 4)                  \n" +
+                              "        (ac/draw-text \"Hello\" 1 3)                  \n" +
+                              "        (ac/draw-text \"Hello\" 2 2)                  \n" +
+                              "        (ac/draw-text \"Hello\" 3 1)                  \n" +
+                              "        (ac/draw-text \"Hello\" 4 0)                  \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("Hello     \n" +
+                     " Hello    \n" +
+                     "  Hello   \n" +
+                     "   Hello  \n" +
+                     "    Hello ", venice.eval(script));
+    }
+
+    @Test
+    public void test_005_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-text \"Hello\" -1 4)                 \n" +
+                              "        (ac/draw-text \"Hello\"  1 3)                 \n" +
+                              "        (ac/draw-text \"Hello\"  3 2)                 \n" +
+                              "        (ac/draw-text \"Hello\"  5 1)                 \n" +
+                              "        (ac/draw-text \"Hello\"  7 0)                 \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("ello      \n" +
+                     " Hello    \n" +
+                     "   Hello  \n" +
+                     "     Hello\n" +
+                     "       Hel", venice.eval(script));
+    }
+
+    @Test
+    public void test_006_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-horizontal-right \"Hello\" -1 4)     \n" +
+                              "        (ac/draw-horizontal-right \"Hello\"  1 3)     \n" +
+                              "        (ac/draw-horizontal-right \"Hello\"  3 2)     \n" +
+                              "        (ac/draw-horizontal-right \"Hello\"  5 1)     \n" +
+                              "        (ac/draw-horizontal-right \"Hello\"  7 0)     \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("ello      \n" +
+                     " Hello    \n" +
+                     "   Hello  \n" +
+                     "     Hello\n" +
+                     "       Hel", venice.eval(script));
+    }
+
+    @Test
+    public void test_007_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-horizontal-left \"Hello\"  9 4)      \n" +
+                              "        (ac/draw-horizontal-left \"Hello\"  7 3)      \n" +
+                              "        (ac/draw-horizontal-left \"Hello\"  5 2)      \n" +
+                              "        (ac/draw-horizontal-left \"Hello\"  3 1)      \n" +
+                              "        (ac/draw-horizontal-left \"Hello\"  1 0)      \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("     olleH\n" +
+                     "   olleH  \n" +
+                     " olleH    \n" +
+                     "lleH      \n" +
+                     "eH        ", venice.eval(script));
+    }
+
+    @Test
+    public void test_008_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-vertical-down \"Hello\"  0 4)      \n" +
+                              "        (ac/draw-vertical-down \"Hello\"  2 3)      \n" +
+                              "        (ac/draw-vertical-down \"Hello\"  4 2)      \n" +
+                              "        (ac/draw-vertical-down \"Hello\"  6 1)      \n" +
+                              "        (ac/draw-vertical-down \"Hello\"  8 0)      \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("H         \n" +
+                     "e H       \n" +
+                     "l e H     \n" +
+                     "l l e H   \n" +
+                     "o l l e H ", venice.eval(script));
+    }
+
+    @Test
+    public void test_009_text() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/draw-vertical-up \"Hello\"  0 0)          \n" +
+                              "        (ac/draw-vertical-up \"Hello\"  2 1)          \n" +
+                              "        (ac/draw-vertical-up \"Hello\"  4 2)          \n" +
+                              "        (ac/draw-vertical-up \"Hello\"  6 3)          \n" +
+                              "        (ac/draw-vertical-up \"Hello\"  8 4)          \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("o l l e H \n" +
+                     "l l e H   \n" +
+                     "l e H     \n" +
+                     "e H       \n" +
+                     "H         ", venice.eval(script));
+    }
+
+    @Test
+    public void test_010_fill() throws Exception {
+        final Venice venice = new Venice();
+
+        final String script = "(do                                                   \n" +
+                              "  (load-module :ascii-canvas ['ascii-canvas :as 'ac]) \n" +
+                              "                                                      \n" +
+                              "  (let [w 10, h 5]                                    \n" +
+                              "    (-> (ac/create w h)                               \n" +
+                              "        (ac/box 0 0 10 5 \"++++-|-|\")                \n" +
+                              "        (ac/fill #\\* 1 1 8 3)                        \n" +
+                              "        (ac/string-ascii))))                          ";
+
+
+        assertEquals("+--------+\n" +
+                     "|********|\n" +
+                     "|********|\n" +
+                     "|********|\n" +
+                     "+--------+", venice.eval(script));
+    }
 }
