6d4a937238c964285aed716d767433e6fbe76c02
jlangch <juerg.lang@ggaweb.ch>
2025-07-28 20:47:18 +0200

    disabled io/watch-dir unit test

--- src/test/java/com/github/jlangch/venice/impl/functions/IOFunctionsTest.java
+++ src/test/java/com/github/jlangch/venice/impl/functions/IOFunctionsTest.java
@@ -33,6 +33,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.github.jlangch.venice.Parameters;
@@ -833,7 +834,7 @@ public class IOFunctionsTest {
     }
 
 
-    // @Disabled   // Java WatchService doesn't work on MacOS !?
+    @Disabled   // Java WatchService doesn't work on MacOS !?
     @Test
     public void test_io_watch_dir() {
         final Venice venice = new Venice();
