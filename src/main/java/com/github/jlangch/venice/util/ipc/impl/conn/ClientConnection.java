7c501ffcb7f36415d0312c5ca593f89aabd83317
jlangch <juerg.lang@ggaweb.ch>
2025-12-21 18:30:44 +0100

    refactoring

--- src/main/java/com/github/jlangch/venice/util/ipc/impl/conn/ClientConnection.java
+++ src/main/java/com/github/jlangch/venice/util/ipc/impl/conn/ClientConnection.java
@@ -36,7 +36,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
-import com.github.jlangch.venice.EofException;
 import com.github.jlangch.venice.TimeoutException;
 import com.github.jlangch.venice.VncException;
 import com.github.jlangch.venice.impl.threadpool.ManagedCachedThreadPoolExecutor;
@@ -198,7 +197,7 @@ public class ClientConnection implements Closeable {
         }
 
         final long start = System.currentTimeMillis();
-        final long limit = start + timeoutMillis;
+//        final long limit = start + timeoutMillis;
 
         try {
             // sending the request message atomically preventing any other thread to
@@ -212,49 +211,51 @@ public class ClientConnection implements Closeable {
                         return null;
                     }
 
-                    final long sendDone = System.currentTimeMillis();
+                    return listener.readResponse((Message)msg, timeoutMillis, opened);
 
-                    // poll the response from the receive queue
-                    while(isOpen()) {
-                        // if a response is ready consume immediately
-                        Message response = listener.poll();
-                        if (msg.hasSameId(response)) {
-                            return response; // the response matches the request
-                        }
-
-                        // check server status
-                        if (!channel.isOpen() || listener.isEOF()) {
-                           break;
-                        }
-
-                        // check response in 80ms steps, to react faster if client or server has closed!!
-                        final long timeout = Math.min(80, limit - System.currentTimeMillis());
-
-                        if (timeout >= 0) {
-                            response = listener.poll(timeout);
-                            if (response == null) {
-                                continue;
-                            }
-                            else if (msg.hasSameId(response)) {
-                                return response; // the response matches the request
-                            }
-                            else {
-                                continue;  // discard out-of-order response
-                            }
-                        }
-                        else {
-                            final String errMsg = String.format(
-                                    "Timeout after %dms (send took %dms) on receiving IPC message response.",
-                                    System.currentTimeMillis() - start,
-                                    sendDone - start);
-                            System.err.println(errMsg);
-                            throw new TimeoutException(errMsg);
-                        }
-                    }
-
-                    final String errMsg = "EOF while receiving IPC message response.";
-                    System.err.println(errMsg);
-                    throw new EofException(errMsg);
+//                    final long sendDone = System.currentTimeMillis();
+//
+//                    // poll the response from the receive queue
+//                    while(isOpen()) {
+//                        // if a response is ready consume immediately
+//                        Message response = listener.poll();
+//                        if (msg.hasSameId(response)) {
+//                            return response; // the response matches the request
+//                        }
+//
+//                        // check server status
+//                        if (!channel.isOpen() || listener.isEOF()) {
+//                           break;
+//                        }
+//
+//                        // check response in 80ms steps, to react faster if client or server has closed!!
+//                        final long timeout = Math.min(80, limit - System.currentTimeMillis());
+//
+//                        if (timeout >= 0) {
+//                            response = listener.poll(timeout);
+//                            if (response == null) {
+//                                continue;
+//                            }
+//                            else if (msg.hasSameId(response)) {
+//                                return response; // the response matches the request
+//                            }
+//                            else {
+//                                continue;  // discard out-of-order response
+//                            }
+//                        }
+//                        else {
+//                            final String errMsg = String.format(
+//                                    "Timeout after %dms (send took %dms) on receiving IPC message response.",
+//                                    System.currentTimeMillis() - start,
+//                                    sendDone - start);
+//                            System.err.println(errMsg);
+//                            throw new TimeoutException(errMsg);
+//                        }
+//                    }
+//
+//                    final String errMsg = "EOF while receiving IPC message response.";
+//                    System.err.println(errMsg);
+//                    throw new EofException(errMsg);
                 }
                 finally {
                     sendSemaphore.release();
