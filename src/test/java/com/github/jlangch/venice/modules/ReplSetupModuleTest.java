effcf84db548deaf0e9a7273926f13ffb89c7c06
jlangch <juerg.lang@ggaweb.ch>
2026-03-12 15:57:50 +0100

    updated to ReplSetupModuleTest v1.12.84

--- src/test/java/com/github/jlangch/venice/modules/ReplSetupModuleTest.java
+++ src/test/java/com/github/jlangch/venice/modules/ReplSetupModuleTest.java
@@ -45,7 +45,7 @@ import com.github.jlangch.venice.impl.util.junit.EnableOnWindows;
 public class ReplSetupModuleTest {
 
     // The Venice version to download from Maven, for staged REPL setup tests
-    private static final String VERSION = "1.12.83";
+    private static final String VERSION = "1.12.84";
 
 
     @Test
