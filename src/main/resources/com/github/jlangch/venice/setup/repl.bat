4a9e144f786192e5e9bba39be76c55ac719fd9e9
jlangch <juerg.lang@ggaweb.ch>
2026-06-28 18:17:19 +0200

    bumped to new version

--- src/main/resources/com/github/jlangch/venice/setup/repl.bat
+++ src/main/resources/com/github/jlangch/venice/setup/repl.bat
@@ -6,7 +6,7 @@ REM # REPL_HOME
 REM #    |
 REM #    +-- libs
 REM #    |    +-- repl.json
-REM #    |    +-- venice-1.13.7.jar
+REM #    |    +-- venice-1.13.8.jar
 REM #    |
 REM #    +-- scripts
 REM #    |    +-- script-1.venice
