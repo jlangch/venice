494f73ae85fb7b82276d1b741212c88c071c41c3
jlangch <juerg.lang@ggaweb.ch>
2026-07-06 22:12:51 +0200

    improving on OpenAI bug demo

--- src/test/java/com/github/jlangch/venice/util/openai/bugs/UsageCostJsonParserBug.java
+++ src/test/java/com/github/jlangch/venice/util/openai/bugs/UsageCostJsonParserBug.java
@@ -33,6 +33,12 @@ import com.openai.models.admin.organization.usage.UsageCostsResponse;
 import com.openai.models.admin.organization.usage.UsageCostsResponse.Data.Result.OrganizationCostsResult;
 
 
+
+/**
+ * Demonstrates the openai-java <code>OrganizationCostsResult</code> value parser bug
+ *
+ * Depends on OpenAI:  com.openai:openai-java:4.41.0
+ */
 public class UsageCostJsonParserBug {
 
     private UsageCostJsonParserBug() {}
@@ -79,57 +85,71 @@ public class UsageCostJsonParserBug {
 
                 final OrganizationCostsResult costs = result.asOrganizationCosts();
 
-                // This custom parser works fine
-                final double value = parseAmount_Workaround(costs);
+                try {
+                    final double value;
 
-                // This throws an exception
-                //final double value = parseAmount_Bug(costs);
+                    if (true) {
+                        // The OpenAI value Json number parser throws an exception
+                        value = costs.amount()
+                                     .flatMap(amount -> amount.value())  // <== exception
+                                     .orElse(0.0);
+                    }
+                    else {
+                        // This custom JsonField number parser works fine
+                        value = Double.parseDouble(
+                                    costs.amount().isPresent()
+                                        ? costs.amount().get()._value().asString().orElse("0.0")
+                                        : "0.0");
+                    }
 
-                final String currency = costs.amount()
-                                            .flatMap(amount -> amount.currency())
-                                            .orElse("unknown");
+                    final String currency = costs.amount()
+                                                .flatMap(amount -> amount.currency())
+                                                .orElse("unknown");
 
-                final String lineItem = costs.lineItem().orElse("-");
+                    final String lineItem = costs.lineItem().orElse("-");
 
-                System.out.println(String.format(
-                        "%s – %s | %.6f %s | %s",
-                        bucketStart,
-                        bucketEnd,
-                        value,
-                        currency,
-                        lineItem));
+                    System.out.println(String.format(
+                            "%s – %s | %.6f %s | %s",
+                            bucketStart,
+                            bucketEnd,
+                            value,
+                            currency,
+                            lineItem));
+                }
+                catch(Exception ex) {
+                    /*
+                       Exception:
 
+                       com.openai.errors.OpenAIInvalidDataException: `value` is invalid, received 0E-6176
+                          at com.openai.core.JsonField.getOptional$openai_java_core(Values.kt:191)
+                          at com.openai.models.admin.organization.usage.UsageCostsResponse$Data$Result$OrganizationCostsResult$Amount.value(UsageCostsResponse.kt:6680)
+
+                       Some exception messages:
+
+                          `value` is invalid, received 0E-6176
+                          `value` is invalid, received 0.003627500000000000000000000000000000
+                          `value` is invalid, received 0.007605000000000000000000000000000000
+
+
+                        Exception in thread "main" com.openai.errors.OpenAIInvalidDataException: `value` is invalid, received 0E-6176
+                            at com.openai.core.JsonField.getOptional$openai_java_core(Values.kt:191)
+                            at com.openai.models.admin.organization.usage.UsageCostsResponse$Data$Result$OrganizationCostsResult$Amount.value(UsageCostsResponse.kt:6680)
+                            at com.github.jlangch.venice.util.openai.bugs.UsageCostJsonParserBug.lambda$2(UsageCostJsonParserBug.java:95)
+                            at java.util.Optional.flatMap(Optional.java:241)
+                            at com.github.jlangch.venice.util.openai.bugs.UsageCostJsonParserBug.lambda$1(UsageCostJsonParserBug.java:95)
+                            at java.util.ArrayList.forEach(ArrayList.java:1259)
+                            at com.github.jlangch.venice.util.openai.bugs.UsageCostJsonParserBug.lambda$0(UsageCostJsonParserBug.java:75)
+                            at java.util.ArrayList.forEach(ArrayList.java:1259)
+                            at com.github.jlangch.venice.util.openai.bugs.UsageCostJsonParserBug.main(UsageCostJsonParserBug.java:71)
+                     */
+                    System.out.println("Exception: " + ex.getMessage());
+
+                    //throw ex;
+                }
             });
         });
     }
 
-    private static double parseAmount_Workaround(final OrganizationCostsResult costs) {
-        // The JSON number parser doesn't work => we roll our own parser!
-        return Double.parseDouble(
-                    costs.amount().isPresent()
-                        ? costs.amount().get()._value().asString().orElse("0.0")
-                        : "0.0");
-    }
-
-    private static double parseAmount_Bug(final OrganizationCostsResult costs) {
-        // This throws an exception
-        return costs.amount()
-                    .flatMap(amount -> amount.value())  // <== exception
-                    .orElse(0.0);
-    }
-
-
-//catch(Exception ex) {
-//    // com.openai.errors.OpenAIInvalidDataException: `value` is invalid, received 0E-6176
-//    //    at com.openai.core.JsonField.getOptional$openai_java_core(Values.kt:191)
-//    //    at com.openai.models.admin.organization.usage.UsageCostsResponse$Data$Result$OrganizationCostsResult$Amount.value(UsageCostsResponse.kt:6680)
-//
-//    // `value` is invalid, received 0E-6176
-//    // `value` is invalid, received 0.003627500000000000000000000000000000
-//    // `value` is invalid, received 0.007605000000000000000000000000000000
-//    // System.out.println(ex.getMessage());
-//    throw ex;
-//}
 
     private static LocalDateTime toLocalDateTime(final long epochSeconds) {
         return LocalDateTime.ofInstant(
